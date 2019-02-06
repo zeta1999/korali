fc607ed347583c332d350bbdf3db8e2c52ee7047
Sergio Martin <martiser@ethz.ch>
2019-02-06 09:50:34 +0100

    Fixing Ackley

--- tests/ackley/ackley.cpp
+++ tests/ackley/ackley.cpp
@@ -11,21 +11,18 @@ double f_Ackley(double *x, int N) {
        s2 += cos(c*x[i]);
    }
 
-   printf("Value: %.12g\n", -a*exp(-b*sqrt(s1/N)) - exp(s2/N) + a + exp(1.));
    return -a*exp(-b*sqrt(s1/N)) - exp(s2/N) + a + exp(1.);
 }
 
 int main(int argc, char* argv[])
 {
-	const size_t nDim = 2;
+	const size_t nDim = 4;
 
   auto problem = Korali::Problem(nDim, f_Ackley, 53754);
 
-//	for (int i = 0; i < 4; i++)	problem[i]->setPriorDistribution("Uniform", -6.0, 6.0);
-	for (int i = 0; i < 4; i++)	problem[i]->setInitialStdDev(2.0);
 	for (int i = 0; i < nDim; i++)	problem[i]->setBounds(-32.0, +32.0);
-	for (int i = 0; i < nDim; i++)	problem[i]->setInitialX(0.0);
-	for (int i = 0; i < nDim; i++)	problem[i]->setInitialStdDev(3.0);
+	for (int i = 0; i < nDim; i++)	problem[i]->setInitialX(5.0 + i);
+	for (int i = 0; i < nDim; i++)	problem[i]->setInitialStdDev(2.0 + i);
 
 	auto solver = Korali::KoraliCMAES(&problem, MPI_COMM_WORLD);
 
