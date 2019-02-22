bf5bb072727315dd6cdb52969c82be1175afc84b
martiser <martiser@ethz.ch>
2019-02-22 08:43:22 +0100

    Broadcasting initial data

--- tests/nCandle/nCandle.cpp
+++ tests/nCandle/nCandle.cpp
@@ -2,29 +2,45 @@
 
 int main(int argc, char* argv[])
 {
-	int problemNumber = 1;
-	for (int i = 0; i < argc; i++) if(!strcmp(argv[i], "-p")) problemNumber = atoi(argv[++i]);
+  int myRank, rankCount;
 
-  // Read solution from file
-  FILE *fid;
-  char pfile[50];
-  sprintf(pfile, "problem%d.in", problemNumber);
-  printf("Running problem from file %s... \n", pfile);
-  fid = fopen(pfile, "r");
+	MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+  MPI_Comm_size(MPI_COMM_WORLD, &rankCount);
 
   pointsInfo p;
-  fscanf(fid, "%lu", &p.nPoints);
-  p.xPos = (double*) calloc (sizeof(double), p.nPoints);
-  p.yPos = (double*) calloc (sizeof(double), p.nPoints);
+  FILE *problemFile;
+
+  if (myRank == 0)
+  {
+		int problemNumber = 1;
+		for (int i = 0; i < argc; i++) if(!strcmp(argv[i], "-p")) problemNumber = atoi(argv[++i]);
+
+		// Read solution from file
+		char pfile[50];
+		sprintf(pfile, "problem%d.in", problemNumber);
+		printf("Running problem from file %s... \n", pfile);
+		problemFile = fopen(pfile, "r");
+		fscanf(problemFile, "%lu", &p.nPoints);
+  }
+
+  MPI_Bcast(&p.nPoints, 1, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
+
+  p.xPos    = (double*) calloc (sizeof(double), p.nPoints);
+  p.yPos    = (double*) calloc (sizeof(double), p.nPoints);
   p.refTemp = (double*) calloc (sizeof(double), p.nPoints);
   p.simTemp = (double*) calloc (sizeof(double), p.nPoints);
 
-  for (int i = 0; i < p.nPoints; i++)
+  if (myRank == 0) for (int i = 0; i < p.nPoints; i++)
   {
-  	fscanf(fid, "%le ", &p.xPos[i]);
-  	fscanf(fid, "%le ", &p.yPos[i]);
-  	fscanf(fid, "%le ", &p.refTemp[i]);
+  	fscanf(problemFile, "%le ", &p.xPos[i]);
+  	fscanf(problemFile, "%le ", &p.yPos[i]);
+  	fscanf(problemFile, "%le ", &p.refTemp[i]);
   }
+
+  MPI_Bcast(p.xPos,    p.nPoints, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Bcast(p.yPos,    p.nPoints, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  MPI_Bcast(p.refTemp, p.nPoints, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
   auto problem = Korali::Problem(heat2DWrapper);
   Korali::Parameter sigma, intensity, width, xPos, yPos;
