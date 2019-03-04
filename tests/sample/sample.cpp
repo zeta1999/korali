3208f67dcb38dd42cf0e20bd4a9ea2162ad447dd
martiser <martiser@ethz.ch>
2019-03-04 16:57:11 +0100

    Adding sampler example

---
+++ tests/sample/sample.cpp
@@ -0,0 +1,32 @@
+#include "korali.h"
+#include "mpi.h"
+
+#define NDIMS 4
+
+double f_Ackley(double *x)
+{
+   double a = 20, b = .2, c = 2.*M_PI, s1 = 0., s2 = 0.;
+
+   for (int i = 0; i < NDIMS; ++i) {
+       s1 += x[i]*x[i];
+       s2 += cos(c*x[i]);
+   }
+
+   return -a*exp(-b*sqrt(s1/NDIMS)) - exp(s2/NDIMS) + a + exp(1.);
+}
+
+int main(int argc, char* argv[])
+{
+  auto problem = Korali::Minimization(f_Ackley);
+
+  Korali::Parameter p;
+  p.setBounds(-32.0, +32.0);
+	for (int i = 0; i < NDIMS; i++) problem.addParameter(p);
+
+  auto Solver = Korali::KoraliCMAES(&problem, MPI_COMM_WORLD);
+	Solver.setStopMinDeltaX(1e-11);
+	Solver.setLambda(128);
+	Solver.run();
+
+	return 0;
+}
