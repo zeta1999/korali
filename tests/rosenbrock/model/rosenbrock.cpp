17e6bbc7ecd5b4e03d75bc534d0c4cbc563693d2
martiser <martiser@ethz.ch>
2019-03-11 15:52:35 +0100

    Some fixes

--- tests/rosenbrock/model/rosenbrock.cpp
+++ tests/rosenbrock/model/rosenbrock.cpp
@@ -8,6 +8,5 @@ double rosenbrock(double *x)
   for (int i = 0; i < NDIMS-1; i++)
       s += 100.*pow(x[i+1]-x[i]*x[i], 2) + pow(x[i]-1., 2);
 
-  printf("[%.3f, %.3f] = %f\n", x[0], x[1], s);
   return s;
 }
