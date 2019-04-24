c8d28691fc7ac235e414f7455c13f15815335fba
wadaniel <wadaniel@ethz.ch>
2019-04-24 16:40:51 +0200

    work on plotting

--- source/python/gui/plot_generations.py
+++ source/python/gui/plot_generations.py
@@ -3,33 +3,62 @@
 import os
 import json
 import time
+import numpy as np
 import matplotlib.pyplot as plt
 
+resultdir = 'korali00000'
 
-outdir = 'korali00000/'
-path = 'korali00000/s00035.json'
-
-idx = 0
-
+numeval  = []
 fval     = []
 sigma    = []
-bestXvec = []
+fvalXvec = []
 
-while( os.path.isfile( 'korali00000/s{0}.json'.format(str(idx).zfill(5))) ):
+plt.figure('CMA-ES live diagonistics: {0}'.format(resultdir),figsize=(8,8))
+#fig, axes = plt.subplots(2, 2, sharex='all')
 
+idx = 1
+while( os.path.isfile( 'korali00000/s{1}.json'.format(resultdir, str(idx).zfill(5))) ):
+
+    path = 'korali00000/s{0}.json'.format(str(idx).zfill(5))
+    
     with open(path) as f:
         data = json.load(f)
         state = data['Solver']['State']
 
+        numeval.append(data['Solver']['Lambda']*idx)
         fval.append(state['CurrentFunctionValue'])  # or maybe BestEver
-        bestXvec.append(state['CurrentBestVector']) # or maybe BestEverVector
+        fvalXvec.append(state['CurrentBestVector']) # or maybe BestEverVector
         sigma.append(state['Sigma'])
 
-    idx = idx+1
     print(idx)
-    print(sigma)
-    print(bestXvec)
 
+    plt.subplot(221)
+    plt.title('Sigma')
+    plt.grid(True)
+    plt.yscale('log')
+    plt.plot(numeval, sigma, color='g')
+    plt.plot(numeval, fval, color='r')
 
+    plt.subplot(222)
+    plt.title('Object Variables')
+    plt.grid(True)
+    plt.plot(numeval, fvalXvec)
+    # TODO: add labes
+
+    plt.subplot(223)
+    plt.title('Scaling (All Main Axes)')
+    plt.grid(True)
+    plt.yscale('log')
+    plt.plot(numeval, numeval) # TODO
+    
+    plt.subplot(224)
+    plt.title('Standard Deviation in All Coordinates')
+    plt.grid(True)
+    plt.yscale('log')
+    plt.plot(numeval, numeval) # TODO
+    
+    plt.pause(0.05) 
     time.sleep(2)
+    idx = idx+1
 
+plt.show()
