39103db457cdb3d61ed5988f6fb32ed2ffd6a41a
wadaniel <wadaniel@ethz.ch>
2019-04-25 15:16:57 +0200

    plot prototype finished

--- source/python/gui/plot_generations.py
+++ source/python/gui/plot_generations.py
@@ -12,6 +12,8 @@ seaborn_avail = False
 try:
     import seaborn as sns
     seaborn_avail = True
+    sns.set_style("darkgrid")
+
 except ImportError:
     print ("WARNING: package \'seaborn\' not available, using default plotting"
            "schemes (install with \'pip install seaborn\')")
@@ -47,21 +49,23 @@ sigma    = []
 fval     = []
 cond     = []
 fvalXvec = []
-Cdiag    = []
+axis     = []
 Csdev    = []
 
-if (seaborn_avail): sns.set_style("darkgrid")
 
-plt.figure('CMA-ES live diagnostics: {0}'.format(resultdir),figsize=(8,8))
+fig = plt.figure('CMA-ES live diagnostics: {0}'.format(resultdir),figsize=(8,8))
 
 idx    = 0
 numdim = 0
 colors = []
 
-while( os.path.isfile( 'korali00000/s{1}.json'.format(resultdir, str(idx).zfill(5))) ):
+while( plt.fignum_exists(fig.number) & os.path.isfile( 'korali00000/s{1}.json'.format(resultdir, str(idx).zfill(5))) ):
 
     path = 'korali00000/s{0}.json'.format(str(idx).zfill(5))
-   
+
+    localtime = time.localtime(time.time())
+    plt.suptitle('CMA-ES: s{0}.json (last update: {1}:{2}:{3})'.format(str(idx).zfill(5), localtime.tm_hour, localtime.tm_min, localtime.tm_sec), fontsize=14)
+
     with open(path) as f:
         data = json.load(f)
         state = data['Solver']['State']
@@ -71,23 +75,22 @@ while( os.path.isfile( 'korali00000/s{1}.json'.format(resultdir, str(idx).zfill(
             colors = hls_colors(numdim)
             for i in range(numdim):
                 fvalXvec.append([])
-                Cdiag.append([])
+                axis.append([])
                 Csdev.append([])
 
             idx = idx + 1
             continue
 
         numeval.append(state['EvaluationCount'])
-        fval.append(state['CurrentBest']) # or maybe BestEver
+        fval.append(state['CurrentBestFunctionValue']) # or maybe BestEver
         sigma.append(state['Sigma'])
         cond.append(state['MaxEigenvalue']/state['MinEigenvalue'])
 
         for i in range(numdim):
             fvalXvec[i].append(state['CurrentBestVector'][i]) # or maybe BestEverVector
-            Cdiag[i].append(state['CovarianceMatrix'][i][i])  # Evals
+            axis[i].append(state['AxisLengths'][i]) # sqrt(Evals)
             Csdev[i].append(sigma[idx-1]*np.sqrt(state['CovarianceMatrix'][i][i]))
 
-    print(idx)
 
     plt.subplot(221)
     plt.title('FunctionValue (red), Sigma (yellow), Condition (blue)')
@@ -105,14 +108,14 @@ while( os.path.isfile( 'korali00000/s{1}.json'.format(resultdir, str(idx).zfill(
     
     if idx == 1:
         legend = ax222.legend(loc='upper center')
-        plt.legend(bbox_to_anchor=(1.04,0.5), loc="center left", borderaxespad=0)
+        plt.legend(bbox_to_anchor=(1.04,0.5), loc="center left", borderaxespad=0, handlelength=1)
 
     ax223 = plt.subplot(223)
     plt.title('Scaling (All Main Axes)')
     plt.grid(True)
     plt.yscale('log')
     for i in range(numdim):
-        ax223.plot(numeval, Cdiag[i], color = colors[i])
+        ax223.plot(numeval, axis[i], color = colors[i])
     
     ax224 = plt.subplot(224)
     plt.title('Standard Deviation in All Coordinates')
@@ -120,10 +123,10 @@ while( os.path.isfile( 'korali00000/s{1}.json'.format(resultdir, str(idx).zfill(
     plt.yscale('log')
     for i in range(numdim):
         ax224.plot(numeval, Csdev[i], color = colors[i], label='X'+str(i+1))
- 
+
     if idx == 1:
         legend = ax224.legend(loc='upper center')
-        plt.legend(bbox_to_anchor=(1.04,0.5), loc="center left", borderaxespad=0)
+        plt.legend(bbox_to_anchor=(1.04,0.5), loc="center left", borderaxespad=0, handlelength=1)
 
    
     plt.pause(0.05) 
