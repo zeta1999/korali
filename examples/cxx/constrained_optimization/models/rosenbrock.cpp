9e343f5b2bd7fa4ee48f953b736eaa48af2097b9
wadaniel <wadaniel@ethz.ch>
2019-05-16 09:24:05 +0200

    updated gitignore:

---
+++ examples/cxx/constrained_optimization/models/rosenbrock.cpp
@@ -0,0 +1,15 @@
+#include "rosenbrock.h"
+#include <stdlib.h>
+#include <math.h>
+
+// Minus Rosenbrock
+void m_rosenbrock(std::vector<double>& pars, std::vector<double>& result)
+{
+ size_t nPars = pars.size();
+
+ double s = 0.;
+ for (size_t i = 0; i < nPars-1; i++)
+  s += 100.0*pow(pars[i+1]-pars[i]*pars[i], 2) + pow(1.0 - pars[i], 2);
+
+ result.push_back(-s);
+}
