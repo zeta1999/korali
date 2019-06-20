09fff0033ee77a185689c85db5c3a4e65f7e70f0
Georgios Arampatzis <garampat@ethz.ch>
2019-06-20 11:36:58 +0200

    found a bug in CCMA-ES initialisation

--- examples/constrained_optimization_cxx/maximize_constrained_g09.cpp
+++ examples/constrained_optimization_cxx/maximize_constrained_g09.cpp
@@ -5,24 +5,27 @@ int main(int argc, char* argv[])
 {
  auto korali = Korali::Engine();
 
- korali["Verbosity"]       = "Detailed";
- korali["Problem"]["Evaluation Type"] = "Direct";
+
+ korali["Problem"] = "Direct Evaluation";
 
  int nParams = 7;
- for (int i = 0; i < nParams; i++)
- {
-  korali["Problem"]["Variables"][i]["Name"] = "X" + std::to_string(i);
-  korali["Problem"]["Variables"][i]["Type"] = "Computational";
-  korali["Solver"]["Variables"][i]["Lower Bound"] = -10.0;
-  korali["Solver"]["Variables"][i]["Upper Bound"] = +10.0;
+ for (int i = 0; i < nParams; i++){
+  korali["Variables"][i]["Name"] = "X" + std::to_string(i);
+  korali["Variables"][i]["Type"] = "Computational";
  }
 
- korali["Solver"]["Method"] = "CCMA-ES";
- korali["Solver"]["Adaption Size"] = 0.1;
- korali["Solver"]["Sample Count"] = 8;
- korali["Solver"]["Viability Sample Count"] = 2;
- korali["Solver"]["Termination Criteria"]["Fitness"]["Value"] = -680.630057374402 - 1e-4;
- korali["Solver"]["Termination Criteria"]["Min DeltaX"]["Value"] = 1e-12;
+ korali["Solver"] = "CCMA-ES";
+
+ for (int i = 0; i < nParams; i++){
+  korali["CCMA-ES"]["Variables"][i]["Lower Bound"] = -10.0;
+  korali["CCMA-ES"]["Variables"][i]["Upper Bound"] = +10.0;
+ }
+
+ korali["CCMA-ES"]["Adaption Size"] = 0.1;
+ korali["CMA-ES"]["Sample Count"] = 8;
+ korali["CMA-ES"]["Viability Sample Count"] = 2;
+ korali["CMA-ES"]["Termination Criteria"]["Fitness"]["Value"] = -680.630057374402 - 1e-4;
+ korali["CMA-ES"]["Termination Criteria"]["Min DeltaX"]["Value"] = 1e-12;
 
  korali.setModel([](Korali::ModelData& d) { g09(d.getVariables(), d.getResults()); });
  korali.addConstraint(g1);
@@ -30,8 +33,10 @@ int main(int argc, char* argv[])
  korali.addConstraint(g3);
  korali.addConstraint(g4);
 
+ korali["Verbosity"]       = "Detailed";
+
  korali.run();
- 
+
  return 0;
 
 }
