ee527470b1bff8b60edb2ce45c7f7bbf736e9298
wadaniel <wadaniel@ethz.ch>
2019-06-20 13:22:03 +0200

    updated example

--- examples/constrained_optimization_cxx/maximize_unconstrained_g09.cpp
+++ examples/constrained_optimization_cxx/maximize_unconstrained_g09.cpp
@@ -5,24 +5,23 @@ int main(int argc, char* argv[])
 {
  auto korali = Korali::Engine();
 
- korali["Verbosity"]       = "Detailed";
- korali["Problem"]["Type"] = "Direct";
+ korali["Problem"]   = "Direct Evaluation";
+ korali["Verbosity"] = "Detailed";
+
+ korali["Solver"] = "CMA-ES";
 
  int nParams = 7;
- for (int i = 0; i < nParams; i++)
- {
-  korali["Problem"]["Variables"][i]["Name"] = "X" + std::to_string(i);
-  korali["Problem"]["Variables"][i]["Type"] = "Computational";
-  korali["Problem"]["Variables"][i]["Distribution"] = "Uniform";
-  korali["Problem"]["Variables"][i]["Minimum"] = -10.0;
-  korali["Problem"]["Variables"][i]["Maximum"] = +10.0;
+ for (int i = 0; i < nParams; i++){
+  korali["Variables"][i]["Name"] = "X" + std::to_string(i);
+  korali["Variables"][i]["CMA-ES"]["Lower Bound"] = -10.0;
+  korali["Variables"][i]["CMA-ES"]["Upper Bound"] = +10.0;
  }
-
- korali["Solver"]["Method"] = "CMA-ES";
- korali["Solver"]["Sample Count"] = 8;
- //korali["Solver"]["Termination Criteria"]["Fitness"]["Value"] = -680.630057374402 - 1e-4;
- korali["Solver"]["Termination Criteria"]["Min DeltaX"]["Value"] = 1e-12;
  
+ korali["CMA-ES"]["Sample Count"] = 8;
+ korali["CMA-ES"]["Termination Criteria"]["Min Fitness"]["Value"] = -680.630057374402 - 1e-4;
+ korali["CMA-ES"]["Termination Criteria"]["Min DeltaX"]["Value"] = 1e-12;
+ 
+
  korali.setModel([](Korali::ModelData& d) { g09_mod(d.getVariables(), d.getResults()); });
 
  korali.run();
