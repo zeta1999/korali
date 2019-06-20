3da483b6165566156e5c35ee3de7c60f1d5e0f24
Georgios Arampatzis <garampat@ethz.ch>
2019-06-20 10:42:04 +0200

    on tutorials

--- tutorial/model/g09.py
+++ tutorial/model/g09.py
@@ -2,18 +2,22 @@
 
 # Minus g09
 
-def g09( pars, result):
+def g09( s ):
   nPars = pars.size();
   if( nPars != 7 )
     print("g09: num pars must be 7\n");
     exit();
+
+  pars = []
+  for i in range(nPars)
+    pars[i] = s.getVariable(i);
 
   res = (pars[0] - 10.0)**2 + 5.0 * (pars[1] - 12.0)**2           \
         + pars[2]**4  + 3.0 * (pars[3] - 11.0)**2                 \
         + 10.0 * pars[4]**6 + 7.0 * pars[5]**2 + pars[6]**4.      \
         - 4.0 * pars[5] * pars[6] - 10.0 * pars[5] - 8.0 * pars[6];
 
-  result.addResult(-res);
+  s.addResult(-res);
 
 
 
@@ -33,7 +37,7 @@ def g3( x, N ):
           - 8.0 * x[6];
 }
 
-double g4( x, N ):
+double g4( x, N):
 {
   return  4.0 * x[0] * x[0] + x[1] * x[1] - 3.0 * x[0] * x[1] \
           + 2.0 * x[2] * x[2] + 5.0 * x[5] - 11.0 * x[6];
