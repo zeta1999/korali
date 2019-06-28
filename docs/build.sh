f5036b4bf353153a0c42cc1431e685006abe158d
Sergio Martin <martiser@ethz.ch>
2019-06-28 11:54:13 +0200

    building script for site

--- docs/build.sh
+++ docs/build.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+function check()
+{
+ if [ ! $? -eq 0 ]
+ then
+  echo "[Korali] Error building site."
+  exit -1
+ fi 
+}
 
+# Installing mkdocs and materials theme
+pip install mkdocs --user
+check
+
+pip install mkdocs-material --user
+check
+
+python3 -m mkdocs build
+check
+
+# Copying website to falcon (gateway)
+scp -r site/* circleci@falcon.ethz.ch:websites/korali
+check
+
+# Copying from falcon to vladimirovich (host)
+ssh circleci@falcon.ethz.ch './update_vladimirovich.sh'
+check
 
 echo "[Korali] Building complete."
