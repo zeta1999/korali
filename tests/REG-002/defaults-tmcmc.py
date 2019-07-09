9c2ce30f974cc74e45a65a6b9d6de23e4b51e8c3
wadaniel <wadaniel@ethz.ch>
2019-07-10 00:48:48 +0200

    REG003 TMCMC

--- tests/REG-002/defaults-tmcmc.py
+++ tests/REG-002/defaults-tmcmc.py
@@ -19,9 +19,9 @@ k["Variables"][0]["Bayesian"]["Prior Distribution"]["Minimum"] = -10.0
 k["Variables"][0]["Bayesian"]["Prior Distribution"]["Maximum"] = +10.0
 
 k["TMCMC"]["Population Size"] = 5000
-#k["Termination Criteria"]["Max Generations"]["Value"] = 1
+k["Termination Criteria"]["Max Generations"] = 0
 
-k["Result Directory"] = "_a2_sampling_tmcmc"
+k["Result Directory"] = "_defaults_tmcmc"
 
 k.run()
 
@@ -29,10 +29,51 @@ k.run()
 
 # Test Configuration
 
+assert_value( k["File Output Frequency"], 1 )
+
+#assert_value( k["TMCMC"]"Terminal Output Frequency"], 1 ) TODO
+
+assert_value( k["TMCMC"]["Covariance Scaling"], 0.04 )
+
+assert_value( k["TMCMC"]["Burn In"], 0 )
+
+assert_value( k["TMCMC"]["Max Rho Update"], 1.0 )
+
+assert_value( k["TMCMC"]["Min Rho Update"], 1e-5 )
+
+assert_value( k["TMCMC"]["Population Size"], 5000 )
+
+assert_boolean( k["TMCMC"]["Use Local Covariance"], False )
+
+
 # Test Internals
+
+assert_value( k["TMCMC"]["Internal"]["Accepted Samples Count"], 5000 ) # what is this (DW)
+
+assert_value( k["TMCMC"]["Internal"]["Annealing Exponent"], 0.0 )
+
+assert_value( k["TMCMC"]["Internal"]["Coefficient of Variation"], 1.0 )
+
+assert_value( k["TMCMC"]["Internal"]["Covariance Matrix"][0], 0.0 )
+
+assert_value( k["TMCMC"]["Internal"]["Proposals Acceptance Rate"], 1.0 )
+
+assert_value( k["TMCMC"]["Internal"]["Selection Acceptance Rate"], 1.0 )
+
 
 # Test Termination Criteria
 
+assert_value( k["Termination Criteria"]["Max Function Evaluations"], 5e7 ) 
+#TODO should be k["MCMC"]["Termination Criteria"]["Max .. "]["Value"] = ..
+
+assert_value( k["Termination Criteria"]["Max Generations"], 0 )
+#TODO should be k["MCMC"]["Termination Criteria"]["Max .. "]["Value"] = ..
+
+
 # Test Variables
 
+assert_value( k["Variables"][0]["Bayesian"]["Prior Distribution"]["Maximum"], 10 )
 
+assert_value( k["Variables"][0]["Bayesian"]["Prior Distribution"]["Minimum"], -10 )
+
+assert_string( k["Variables"][0]["Bayesian"]["Prior Distribution"]["Type"], "Uniform" )
