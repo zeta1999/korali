44a28b3c6ceae4dc4c7facef8da6692f5fe7891e
Sergio Martin <martiser@ethz.ch>
2019-07-11 13:11:13 +0200

    Fixing MPI test

--- tests/UNIT-001/run_test.sh
+++ tests/UNIT-001/run_test.sh
@@ -9,10 +9,13 @@
 # Steps: 
 # 1 - Operation: Run 8x1 distribution.
 #     Expected result: 8 Concurrent teams of 1 MPI rank run with rc = 0.
+#     If MPI is not installed (e.g., macOs) , it will not run.
 # 2 - Operation: Run 4x2 distribution.
 #     Expected result: 4 Concurrent teams of 2 MPI ranks run with rc = 0.
+#     If MPI is not installed (e.g., macOs) , it will not run.
 # 3 - Operation: Run 1x8 distribution.
 #     Expected result: 1 Team of 8 MPI ranks run with rc = 0.  
+#     If MPI is not installed (e.g., macOs) , it will not run.
 ###############################################################################
 
 ###### Auxiliar Functions and Variables #########
@@ -38,6 +41,12 @@ function logEcho ()
 }
 
 ############# STEP 1 ##############
+
+if [[ $MPICXX == "" ]]
+then
+ echo "[Korali] MPI not installed, skipping test."
+ exit 0
+fi
 
 logEcho "[Korali] Running mpirun -n 8 ./heat2d_posterior..."
 mpirun -n 8 ./heat2d_posterior >> $logFile 2>&1
