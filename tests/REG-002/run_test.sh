edf72500401b30a6968aaf5adffef7c365166ddf
wadaniel <wadaniel@ethz.ch>
2019-07-15 16:28:42 +0200

    cleanup test descriptions:

--- tests/REG-002/run_test.sh
+++ tests/REG-002/run_test.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
 ##############################################################################
-# Brief: Re-run all tutorials for basic sanity check. 
+# Brief: Check solver default values 
 # Type: Regression Test 
 # Description:
-# This test finds and runs tutorials in the /tutorials folder to make sure
-# the typical use cases still work.
-# Steps: 
-# 1 - Operation: List and run all .py scripts in the tutorials/python folder.
-#     Expected result: all of the .py scripts will run for less than 20 secs
-#     and rc = 0.  
-# 2 - Operation: List, compile, and run all .cpp files in the tutorials/cxx
-#     folder. 
-#     Expected result: all of the .py scripts will run for less than 20
-#     secs and rc = 0.
-# 3 - Operation: plot all results produced in Step 1. 
-#     Expected result: all plotting scripts will run for less than 30 secs and
-#     rc = 0.
+# This test instantiates all solvers in a secondary python script and 
+# valdiates the default values.
 ###############################################################################
 
 ###### Auxiliar Functions and Variables #########
@@ -25,7 +14,7 @@ source ../functions.sh
 
 ############# STEP 1 ##############
 
-logEcho "[Korali] Beginning python tests"
+logEcho "[Korali] Beginning defaults tests"
 
 for file in *.py
 do
@@ -36,19 +25,3 @@ do
   check_result
 done
 
-############# STEP 2 ##############
-
-#make clean >> $logFile
-#check_result
-
-#make -j 4 >> $logFile 
-#check_result
-
-#for file in *.cpp
-#do
-#  logEcho "-------------------------------------"
-#  logEcho " Running $file"
-#  logEcho "-------------------------------------"
-#  ./"${file%.*}" >> $logFile
-#  check_result
-#done
--- tests/REG-003/run_test.sh
+++ tests/REG-003/run_test.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
 ##############################################################################
-# Brief: Re-run all tutorials for basic sanity check. 
+# Brief: Compare python and cxx results
 # Type: Regression Test 
 # Description:
-# This test finds and runs tutorials in the /tutorials folder to make sure
-# the typical use cases still work.
-# Steps: 
-# 1 - Operation: List and run all .py scripts in the tutorials/python folder.
-#     Expected result: all of the .py scripts will run for less than 20 secs
-#     and rc = 0.  
-# 2 - Operation: List, compile, and run all .cpp files in the tutorials/cxx
-#     folder. 
-#     Expected result: all of the .py scripts will run for less than 20
-#     secs and rc = 0.
-# 3 - Operation: plot all results produced in Step 1. 
-#     Expected result: all plotting scripts will run for less than 30 secs and
-#     rc = 0.
+# This test compares the results (json files) produced in /tutorials/python/ 
+# with the results from /tutorials/cxx/
 ###############################################################################
 
 ###### Auxiliar Functions and Variables #########
@@ -25,7 +14,7 @@ source ../functions.sh
 
 ############# STEP 1 ##############
 
-logEcho "[Korali] Beginning python tests"
+logEcho "[Korali] Beginning comparison tests"
 
 for file in *.py
 do
@@ -35,20 +24,3 @@ do
   ./"$file" >> $logFile
   check_result
 done
-
-############# STEP 2 ##############
-
-#make clean >> $logFile
-#check_result
-
-#make -j 4 >> $logFile 
-#check_result
-
-#for file in *.cpp
-#do
-#  logEcho "-------------------------------------"
-#  logEcho " Running $file"
-#  logEcho "-------------------------------------"
-#  ./"${file%.*}" >> $logFile
-#  check_result
-#done
