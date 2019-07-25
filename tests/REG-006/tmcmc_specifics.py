fd092076413f103a28214dff1c455511735b6b1c
wadaniel <wadaniel@ethz.ch>
2019-07-25 15:48:20 +0200

    added reg6 test

---
+++ tests/REG-006/tmcmc_specifics.py
@@ -0,0 +1,65 @@
+#!/bin/bash
+import os
+import sys
+import json
+import korali
+
+sys.path.append('./helpers')
+from reg006_helpers import *
+
+#################################################
+# Set Up  TMCMC run
+#################################################
+
+print("[Korali] Prepare TMCMC run")
+
+# Starting Korali's Engine
+k = korali.initialize()
+
+k["Problem"]["Type"] = "Sampling"
+
+k["Variables"][0]["Name"] = "X"
+k["Variables"][0]["Prior Distribution"]["Type"] = "Uniform"
+k["Variables"][0]["Prior Distribution"]["Minimum"] = -10.0
+k["Variables"][0]["Prior Distribution"]["Maximum"] = +10.0
+
+k["Solver"]["Type"] = "TMCMC"
+k["Solver"]["Population Size"] = 5000
+k["Solver"]["Covariance Scaling"] = 0.001
+
+k["Solver"]["Default Burn In"] = 5
+k["Solver"]["Burn In Steps"][0] = 10
+k["Solver"]["Burn In Steps"][1] = 7
+
+k["General"]["Random Seed"] = 314
+
+k.setModel(evaluateModel)
+
+#################################################
+#  Run TMCMC
+#################################################
+
+print("[Korali] Run TMCMC..")
+k.run()
+
+#################################################
+# Read Results
+#################################################
+
+print("[Korali] Read & Evaluate Output..")
+src = "_korali_result"
+resultfiles = [f for f in os.listdir(src) if os.path.isfile(os.path.join(src, f))]
+resultfiles = sorted(resultfiles) 
+
+for filename in resultfiles:
+  path   = '{0}/{1}'.format(src, filename)
+        
+  with open(path) as f:
+    data  = json.load(f)
+
+#################################################
+# Test Results
+#################################################
+
+    assert_value( data['Solver']['Default Burn In'], 5 )
+
