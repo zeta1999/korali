3685750340b9659a9068c13af8932b9b8332fb8e
Sergio Martin <martiser@ethz.ch>
2019-08-16 13:40:44 +0200

    Continuing korali jsonize

--- source/python/builder/buildKorali.py
+++ source/python/builder/buildKorali.py
@@ -29,6 +29,44 @@ def buildKorali(koraliDir):
  newHeaderString = newHeaderString.replace('// Include Files', headerFilesString)
  
  ##### Processing code file
+ 
+ #### Parsing Conduits
+
+ conduitDetectString= ''
+ for root, dirs, files in os.walk(koraliDir + '/conduits/'):
+  for file in files:
+   if file.endswith(".json"):
+    filePath = os.path.join(root, file)
+    with open(filePath, 'r') as file: conduitConfig = json.load(file)
+    conduitDetectString += ' if (_conduitType == "' + conduitConfig['Alias'] + '")  _conduit = new Korali::Conduit::' + conduitConfig['Class'] + '();\n' 
+ 
+ newCodeString = newCodeString.replace(' // Configuring Conduit', conduitDetectString)
+ 
+ #### Parsing Problems
+
+ problemDetectString= ''
+ for root, dirs, files in os.walk(koraliDir + '/problems/'):
+  for file in files:
+   if file.endswith(".json"):
+    filePath = os.path.join(root, file)
+    with open(filePath, 'r') as file: problemConfig = json.load(file)
+    problemDetectString += ' if (_problemType == "' + problemConfig['Alias'] + '")  _problem = new Korali::Problem::' + problemConfig['Class'] + '();\n' 
+ 
+ newCodeString = newCodeString.replace(' // Configuring Problem', problemDetectString)
+ 
+ #### Parsing Solvers
+
+ solverDetectString= ''
+ for root, dirs, files in os.walk(koraliDir + '/solvers/'):
+  for file in files:
+   if file.endswith(".json"):
+    filePath = os.path.join(root, file)
+    with open(filePath, 'r') as file: solverConfig = json.load(file)
+    solverDetectString += ' if (_solverType == "' + solverConfig['Alias'] + '")  _solver = new Korali::Solver::' + solverConfig['Class'] + '();\n' 
+ 
+ newCodeString = newCodeString.replace(' // Configuring Solver', solverDetectString)
+   
+ ##### Saving Output Files
    
  print('[Korali] Creating: ' + koraliNewHeaderFile + '...')
  with open(koraliNewHeaderFile, 'w') as file: file.write(newHeaderString)
