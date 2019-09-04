2a6b5d32f4b6d1738a80fec779abfdcb6554b913
Sergio Martin <martiser@ethz.ch>
2019-09-04 16:15:43 +0200

    Continuing refactoring

--- tutorials/b3-running-cxx/run-cmaes.cpp
+++ tutorials/b3-running-cxx/run-cmaes.cpp
@@ -20,32 +20,32 @@ int main(int argc, char* argv[])
  k["Variables"][0]["Prior Distribution"]["Type"] = "Uniform";
  k["Variables"][0]["Prior Distribution"]["Minimum"] = 10.0;
  k["Variables"][0]["Prior Distribution"]["Maximum"] = 60.0;
- k["Variables"][0]["Lower Bound"] = 10.0;
- k["Variables"][0]["Upper Bound"] = 60.0;
+ k["Variables"][0]["Initial Mean"] = 30.0;
+ k["Variables"][0]["Initial Standard Deviation"] = 5.0;
 
  k["Variables"][1]["Name"] = "PosX";
  k["Variables"][1]["Bayesian Type"] = "Computational";
  k["Variables"][1]["Prior Distribution"]["Type"] = "Uniform";
  k["Variables"][1]["Prior Distribution"]["Minimum"] = 0.0;
  k["Variables"][1]["Prior Distribution"]["Maximum"] = 0.5;
- k["Variables"][1]["Lower Bound"] = 0.0;
- k["Variables"][1]["Upper Bound"] = 0.5;
+ k["Variables"][1]["Initial Mean"] = 0.25;
+ k["Variables"][1]["Initial Standard Deviation"] = 0.01;
 
  k["Variables"][2]["Name"] = "PosY";
  k["Variables"][2]["Bayesian Type"] = "Computational";
  k["Variables"][2]["Prior Distribution"]["Type"] = "Uniform";
  k["Variables"][2]["Prior Distribution"]["Minimum"] = 0.6;
  k["Variables"][2]["Prior Distribution"]["Maximum"] = 1.0;
- k["Variables"][2]["Lower Bound"] = 0.6;
- k["Variables"][2]["Upper Bound"] = 1.0;
+ k["Variables"][2]["Initial Mean"] = 0.8;
+ k["Variables"][2]["Initial Standard Deviation"] = 0.1;
 
  k["Variables"][3]["Name"] = "Sigma";
  k["Variables"][3]["Bayesian Type"] = "Statistical";
  k["Variables"][3]["Prior Distribution"]["Type"] = "Uniform";
  k["Variables"][3]["Prior Distribution"]["Minimum"] = 0.0;
  k["Variables"][3]["Prior Distribution"]["Maximum"] = 20.0;
- k["Variables"][3]["Lower Bound"] = 0.0;
- k["Variables"][3]["Upper Bound"] = 20.0;
+ k["Variables"][3]["Initial Mean"] = 10.0;
+ k["Variables"][3]["Initial Standard Deviation"] = 1.0;
 
  k.run();
 
