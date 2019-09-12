4df054cdde6ab9967591fb9018332739a8f99e20
Sergio Martin <martiser@ethz.ch>
2019-09-12 12:58:20 +0200

    Fixing PSI problem

--- tutorials/a6-simple-hierarchical-bayesian/phase2/x.py
+++ tutorials/a6-simple-hierarchical-bayesian/phase2/x.py
@@ -22,27 +22,25 @@ kH["Problem"]["Conditional Priors"][1]["Type"] = "LogNormal"
 kH["Problem"]["Conditional Priors"][1]["Mu"]    = "Psi 3"
 kH["Problem"]["Conditional Priors"][1]["Sigma"] = "Psi 4"
 
+kH["Variables"][0]["Name"] = "Psi 1"
+kH["Variables"][0]["Prior Distribution"]["Type"] = "Uniform"
+kH["Variables"][0]["Prior Distribution"]["Minimum"] =  0.0
+kH["Variables"][0]["Prior Distribution"]["Maximum"] = 20.0
 
-kH["Problem"]["Variables"][0]["Name"] = "Psi 1"
-kH["Problem"]["Variables"][0]["Prior Distribution"]["Type"] = "Uniform"
-kH["Problem"]["Variables"][0]["Prior Distribution"]["Minimum"] =  0.0
-kH["Problem"]["Variables"][0]["Prior Distribution"]["Maximum"] = 20.0
+kH["Variables"][1]["Name"] = "Psi 2"
+kH["Variables"][1]["Prior Distribution"]["Type"] = "Uniform"
+kH["Variables"][1]["Prior Distribution"]["Minimum"] =  0.0
+kH["Variables"][1]["Prior Distribution"]["Maximum"] = 20.0
 
-kH["Problem"]["Variables"][1]["Name"] = "Psi 2"
-kH["Problem"]["Variables"][1]["Prior Distribution"]["Type"] = "Uniform"
-kH["Problem"]["Variables"][1]["Prior Distribution"]["Minimum"] =  0.0
-kH["Problem"]["Variables"][1]["Prior Distribution"]["Maximum"] = 20.0
+kH["Variables"][2]["Name"] = "Psi 3"
+kH["Variables"][2]["Prior Distribution"]["Type"] = "Uniform"
+kH["Variables"][2]["Prior Distribution"]["Minimum"] = -2.0
+kH["Variables"][2]["Prior Distribution"]["Maximum"] = +2.0
 
-kH["Problem"]["Variables"][2]["Name"] = "Psi 3"
-kH["Problem"]["Variables"][2]["Prior Distribution"]["Type"] = "Uniform"
-kH["Problem"]["Variables"][2]["Prior Distribution"]["Minimum"] = -2.0
-kH["Problem"]["Variables"][2]["Prior Distribution"]["Maximum"] = +2.0
-
-kH["Problem"]["Variables"][3]["Name"] = "Psi 4"
-kH["Problem"]["Variables"][3]["Prior Distribution"]["Type"] = "Uniform"
-kH["Problem"]["Variables"][3]["Prior Distribution"]["Minimum"] = 0.0
-kH["Problem"]["Variables"][3]["Prior Distribution"]["Maximum"] = 5.0
-
+kH["Variables"][3]["Name"] = "Psi 4"
+kH["Variables"][3]["Prior Distribution"]["Type"] = "Uniform"
+kH["Variables"][3]["Prior Distribution"]["Minimum"] = 0.0
+kH["Variables"][3]["Prior Distribution"]["Maximum"] = 5.0
 
 kH["Solver"]["Type"] = "TMCMC"
 kH["Solver"]["Population Size"] = 2000
@@ -50,7 +48,7 @@ kH["Solver"]["Default Burn In"] = 1;
 kH["Solver"]["Target Coefficient Of Variation"] = 0.6
 kH["Solver"]["Covariance Scaling"] = 0.1
 
-kH["General"]["Console Output"]["Verbosity"] = "Detailed"
-kH["General"]["Results Output"]["Path"] = "../setup/results_phase_2/"
+kH["Console Output"]["Verbosity"] = "Detailed"
+kH["Results Output"]["Path"] = "../setup/results_phase_2/"
 
 kH.run()
