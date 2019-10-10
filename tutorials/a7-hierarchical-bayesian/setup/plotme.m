7812d1355d395b513a5548facd83487434eeb884
Georgios Arampatzis <garampat@ethz.ch>
2019-10-10 17:54:59 +0200

    increase the number of samples in a7, phase 3a.  solves #302

--- tutorials/a7-hierarchical-bayesian/setup/plotme.m
+++ tutorials/a7-hierarchical-bayesian/setup/plotme.m
@@ -1,19 +1,19 @@
 % Add the following to your startup.m:
 % addpath(genpath('KORALI_PATH/source/matlab/'))
 
+% file = 'results_phase_1/004/final.json';
 % file = 'results_phase_2/final.json';
-% file = 'results_phase_2/final.json';
-% file = 'results_phase_3a/final.json';
-file = 'results_phase_3b/final.json';
+file = 'results_phase_3a/final.json';
+% file = 'results_phase_3b/final.json';
 
 data = jsondecode(fileread(file));
 
-N = length(data.Problem.Variables); 
+N = length(data.Variables); 
 
 Ns = data.Solver.PopulationSize; 
 
-x = reshape( data.Solver.Internal.SampleDatabase, N, Ns )';
-y = data.Solver.Internal.SampleFitnessDatabase;
+x = data.Solver.Internal.SampleDatabase;
+y = data.Solver.Internal.SampleLogLikelihoodDatabase;
 
 figure(1); clf
 [ h, ax, BigAx, hhist, pax ] = plotmatrix_hist( x , y );
