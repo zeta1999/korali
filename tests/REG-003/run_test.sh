e6136d37af52353a05b237581fa22febf34c37ec
wadaniel <wadaniel@ethz.ch>
2019-10-10 09:45:08 +0200

    updated REG003

--- tests/REG-003/run_test.sh
+++ tests/REG-003/run_test.sh
@@ -15,7 +15,9 @@ fi
 # Clone korali-apps repository
 #################################################
 
+logEcho ""
 logEcho "[Korali] Cloning korali-apps repository"                                   
+
 rm -rf korali-apps
 git clone git://github.com/cselab/korali-apps.git >> $logFile
 check_result
@@ -24,19 +26,22 @@ check_result
 # Test Korali+LAMMPS
 #################################################
 
-pushd korali-apps/LAMMPS
+
+logEcho ""
 logEcho "[Korali] Testing Korali+LAMMPS..."
 
+pushd korali-apps/LAMMPS
+
 logEcho "[Korali] Converting to dry run..."
-cat optimize.py | sed -e 's/run()/dry()/g' > optimize_dry.py
+cat run-tmcmc.py | sed -e 's/run()/dry()/g' > sample_dry.py
 check_result
 
 logEcho "[Korali] Setting permissions..."
-chmod a+x optimize_dry.py
+chmod a+x sample_dry.py
 check_result
 
-logEcho "[Korali] Running optimize_dry.py..."
-./optimize_dry.py >> $logFile
+logEcho "[Korali] Running sample_dry.py..."
+./sample_dry.py 2 >> $logFile
 check_result
 
 popd
@@ -45,20 +50,31 @@ popd
 # Test Korali+Mirheo
 #################################################
 
+logEcho ""
 logEcho "[Korali] Testing Korali+Mirheo..."
 
 pushd korali-apps/mirheo/rbc_stretching
 
 logEcho "[Korali] Converting to dry run..."
-cat run.py | sed -e 's/run()/dry()/g' > run_dry.py
+cat run-cmaes.py | sed -e 's/run()/dry()/g' > cmaes_dry.py
+check_result
+
+cat run-tmcmc.py | sed -e 's/run()/dry()/g' > tmcmc_dry.py
 check_result
 
 logEcho "[Korali] Setting permissions..."
-chmod a+x run_dry.py
+chmod a+x cmaes_dry.py
 check_result
 
-logEcho "[Korali] Running run_dry.py..."
-mpirun -n 3 ./run_dry.py >> $logFile
+chmod a+x tmcmc_dry.py
+check_result
+
+logEcho "[Korali] Running cmaes_dry.py..."
+mpirun -n 3 ./cmaes_dry.py 2 >> $logFile
+check_result
+
+logEcho "[Korali] Running tmcmc_dry.py..."
+mpirun -n 3 ./tmcmc_dry.py 2 >> $logFile
 check_result
 
 popd
@@ -67,6 +83,7 @@ popd
 # Test Korali+MSolve
 #################################################
 
+logEcho ""
 logEcho "[Korali] Testing Korali+MSolve (Heat EQ)..."
 
 pushd korali-apps/MSolve/heatEq
@@ -80,7 +97,7 @@ chmod a+x run_dry.py
 check_result
 
 logEcho "[Korali] Running run_dry.py..."
-mpirun -n 3 ./run_dry.py >> $logFile
+mpirun -n 3 ./run_dry.py 2 >> $logFile
 check_result
 
 popd
