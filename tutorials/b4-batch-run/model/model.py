e70ee1a7494b945c809a98dd9af68a435ffa3ef8
Sergio Martin <martiser@ethz.ch>
2019-10-17 13:28:28 +0200

    Adding support for multiple runs

---
+++ tutorials/b4-batch-run/model/model.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Single function evaluation
+def model(p):
+  x = p["Parameters"][0]
+  p["Evaluation"] = -0.5*x*x
+
+# Function and Gradient function evaluation
+def model_with_gradient(p):
+  X = p["Parameters"];
+  gradient = [];
+  evaluation = 0
+  for x in X:
+    evaluation  += -0.5*x*x
+    gradient.append( -x )
+
+  p["Evaluation"] = evaluation
+  p["Gradient"]   = gradient;
---
+++ tutorials/b4-batch-run/run-cmaes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+## In this example, we demonstrate how Korali finds values for the
+## variables that maximize the objective function, given by a
+## user-provided computational model.
+
+# Importing computational model
+import sys
+sys.path.append('./model')
+from model import *
+
+# Starting Korali's Engine
+import korali
+k = korali.initialize()
+
+# Configuring Problem.
+k["Problem"]["Type"] = "Evaluation/Direct/Basic"
+k["Problem"]["Objective"] = "Maximize"
+k["Problem"]["Objective Function"] = model
+
+# Defining the problem's variables.
+k["Variables"][0]["Name"] = "X"
+k["Variables"][0]["Lower Bound"] = -10.0
+k["Variables"][0]["Upper Bound"] = +10.0
+
+# Configuring CMA-ES parameters
+k["Solver"]["Type"] = "Optimizer/CMAES"
+k["Solver"]["Population Size"] = 32
+k["Solver"]["Termination Criteria"]["Min Value Difference Threshold"] = 1e-7
+k["Solver"]["Termination Criteria"]["Max Generations"] = 100
+
+# Running Multiple Korali Instances
+k.runMultiple(16)
