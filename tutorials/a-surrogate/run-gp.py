fe2a46590c4579e31d22e40f9f7bd36aede23e5c
Georgios Arampatzis <garampat@ethz.ch>
2019-10-18 16:57:22 +0200

    train GP works

--- tutorials/a-surrogate/run-gp.py
+++ tutorials/a-surrogate/run-gp.py
@@ -5,14 +5,30 @@ import sys
 import numpy as np
 import korali
 
+
+f = open( 'data/simple.txt', "r" )
+data = [ [int(n) for n in line.split()]  for line in f]
+f.close()
+x = [row[:-1] for row in data]
+y = [row[ -1] for row in data]
+
+
+
+
+n = 40;
+x = [[i] for i in range(n)]
+y = [2*i+1 for i in range(n)]
+
 k = korali.initialize()
 
 k["Problem"]["Type"] = "Evaluation/GaussianProcess"
-k["Problem"]["Covariance Function"] = "George"
+k["Problem"]["Covariance Function"] = "CovSum ( CovSEiso, CovNoise)"
+k["Problem"]["X Data"] = x
+k["Problem"]["Y Data"] = y
 
 # Configuring CMA-ES parameters
 k["Solver"]["Type"] = "Optimizer/Rprop"
-k["Solver"]["Termination Criteria"]["Max Generations"] = 20
+k["Solver"]["Termination Criteria"]["Max Generations"] = 200
 k["Solver"]["Termination Criteria"]["Parameter Relative Tolerance"] = 1e-8
 
 
