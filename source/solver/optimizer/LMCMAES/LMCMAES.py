609173f50984febfb69e636675c7ddea71b8e0f1
Sergio Martin <martiser@ethz.ch>
2019-11-05 11:06:11 +0100

    Adapted LMCMAES

--- source/solver/optimizer/LMCMAES/LMCMAES.py
+++ source/solver/optimizer/LMCMAES/LMCMAES.py
@@ -3,116 +3,66 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
-
-from korali.fileIO import *
 from korali.plotter.helpers import hlsColors, pauseLight, drawMulticoloredLine, checkFigure
-from korali.plotter.helpers import verifyGeneration, initDefaults, getStateAndGeneration, appendStates, appendStateVectors
 
 # Plot CMAES results (read from .json files)
-def plot(src, plotAll=False, live=False, generation=None, test=False, plot_mean=False ):
-    plt.style.use('seaborn-dark')
- 
-    verifyGeneration(generation, 3)
+def plot(js):
+    fig, ax = plt.subplots(2,2,num='Korali Results', figsize=(8,8))
+    solver = js['Solvers'][0]['Type']
+    numdim = len(js['Variables'])
+    names  = [ js['Variables'][i]['Name'] for i in range(numdim) ]
     
-    stateNames = ['Current Best Value', 'Best Ever Value', 'Sigma', 'Conjugate Evolution Path L2 Norm']
-    vecStateNames = ['Current Best Variables', 'Current Mean', 'Evolution Path Weights']
+    fval = []
+    dfval = []
+    gen = []
+    sigma = []
+    psL2 = []
+    objVec = []
+    evoweights = []
 
-    names, colors, numeval, sigma, psL2, dfval, fval, best, fvalXvec, mu, evoweights = ([] for i in range(11))
- 
-    resultfiles = getResultFiles(src, 0, generation)
-    if (resultfiles == []):
-      print("[Korali] Error: Did not find Korali results in the folder...".format(src))
-      exit(-1)
+    for s in js['Solvers']:
+     fval.append(s['Internal']['Current Best Value'])
+     dfval.append(abs(s['Internal']['Current Best Value'] - s['Internal']['Best Ever Value']))
+     gen.append(s['Internal']['Current Generation'])
+     sigma.append(s['Internal']['Sigma'])
+     psL2.append(s['Internal']['Conjugate Evolution Path L2 Norm'])
+     objVec.append(s['Internal']['Current Best Variables'])
+     evoweights.append(s['Internal']['Evolution Path Weights'])
 
-    solverName, names, numdim, gen = initDefaults(src, "initial.json", (fvalXvec, mu, evoweights))
-    colors = hlsColors(numdim)
-
-    updateLegend = live or plotAll
-    
-    fig, ax = plt.subplots(2,2,num='{0} live diagnostics'.format(solverName), figsize=(8,8))
-    if (updateLegend):
-        fig.show()
-
-    while True:
-        for filename in resultfiles:
-            path = '{0}/{1}'.format(src, filename)
-            
-            with open(path) as f:
-                data = json.load(f)
-                state, gen = getStateAndGeneration(data)
-      
-                if updateLegend:
-                    checkFigure(fig.number)
-       
-                if gen > 1:
-    
-                    appendStates(state, (fval, best, sigma, psL2), stateNames)
-                    appendStateVectors(state, (fvalXvec, mu, evoweights), vecStateNames)
-
-                    numeval.append(data['Conduit']['Internal']['Model Evaluation Count'])
-                    dfval.append(abs(state['Current Best Value'] - state['Best Ever Value']))
-
-                    if (plotAll == True):
-                        draw_figure(fig, ax, src, gen, numeval, numdim, fval, 
-                                dfval, sigma, psL2, fvalXvec, mu, evoweights,
-                                colors, names, plot_mean, updateLegend)
-     
-        checkFigure(fig.number)
-        draw_figure(fig, ax, src, gen, numeval, numdim, fval, dfval,
-                sigma, psL2, fvalXvec, mu, evoweights, colors, names,
-                plot_mean, updateLegend)
-            
-        if live == False:
-            break
-
-        resultfiles = getResultFiles(src, gen+1, generation, False)
-        if (resultfiles == []):
-         print("[Korali] Error: Did not find Korali results in the folder...".format(src))
-         exit(-1)
-     
-    plt.show()
-    print("[Korali] Figure closed - Bye!")
-
-
-# Create Plot from Data
-def draw_figure(fig, ax, src, idx, numeval, numdim, fval, dfval, sigma,
-        psL2, fvalXvec, mu, evoweights, colors, names, plot_mean, updateLegend):
-
-    plt.suptitle( 'Generation {0}'.format(str(idx).zfill(5)),\
-                      fontweight='bold',\
-                      fontsize=12 )
+    plt.suptitle('CMAES Diagnostics', fontweight='bold', fontsize=12 )
 
     # Upper Left Plot
     ax[0,0].grid(True)
     ax[0,0].set_yscale('log')
-    drawMulticoloredLine(ax[0,0], numeval, fval, 0.0, 'r', 'b', '$| F |$')
-    ax[0,0].plot(numeval, dfval, 'x', color = '#34495e', label = '$| F - F_{best} |$')
-    ax[0,0].plot(numeval, sigma, color='#F8D030', label = '$\sigma$')
-    ax[0,0].plot(numeval, psL2,  color='k', label = '$|| \mathbf{p}_{\sigma} ||$')
-    if ( (idx == 2) or (updateLegend == False) ):
-        ax[0,0].legend(bbox_to_anchor=(0,1.00,1,0.2), loc="lower left", mode="expand", ncol = 3, handlelength=1, fontsize = 8)
+    drawMulticoloredLine(ax[0,0], gen, fval, 0.0, 'r', 'b', '$| F |$')
+    ax[0,0].plot(gen, dfval, 'x', color = '#34495e', label = '$| F - F_{best} |$')
+    ax[0,0].plot(gen, sigma, color='#F8D030', label = '$\sigma$')
+    ax[0,0].plot(gen, psL2,  color='k', label = '$|| \mathbf{p}_{\sigma} ||$')
+    
+    #if ( (idx == 2) or (updateLegend == False) ):
+    ax[0,0].legend(bbox_to_anchor=(0,1.00,1,0.2), loc="lower left", mode="expand", ncol = 3, handlelength=1, fontsize = 8)
 
+    colors = hlsColors(numdim)
+    
     # Upper Right Plot
-    objVec = []
-    if (plot_mean):
-        ax[0,1].set_title('Mean of Objective Variables')
-        objVec = mu
-    else:
-        ax[0,1].set_title('Objective Variables')
-        objVec = fvalXvec
-
+    #if (plot_mean):
+    #    ax[0,1].set_title('Mean of Objective Variables')
+    #    objVec = mu
+    #else:
+    ax[0,1].set_title('Objective Variables')
     ax[0,1].grid(True)
     for i in range(numdim):
-        ax[0,1].plot(numeval, objVec[i], color = colors[i], label=names[i])
-    if ( (idx == 2) or (updateLegend == False) ):
-        ax[0,1].legend(bbox_to_anchor=(1.04,0.5), loc="center left", borderaxespad=0, handlelength=1)
+        ax[0,1].plot(gen, objVec, color = colors[i], label=names[i])
+   
+    #if ( (idx == 2) or (updateLegend == False) ):
+    ax[0,1].legend(bbox_to_anchor=(1.04,0.5), loc="center left", borderaxespad=0, handlelength=1)
 
     # Lower Right Plot
     ax[1,0].set_title('Evolution Path Weights')
     ax[1,0].grid(True)
     ax[1,0].set_yscale('log')
     for i in range(numdim):
-        ax[1,0].plot(numeval, evoweights[i], color = colors[i])
+        ax[1,0].plot(gen, evoweights, color = colors[i])
 
     # Lower Left Plot
     ax[1,1].set_title('$\sigma \sqrt{diag(\mathbf{C})}$')
@@ -120,3 +70,4 @@ def draw_figure(fig, ax, src, idx, numeval, numdim, fval, dfval, sigma,
     ax[1,1].set_yscale('log')
         
     pauseLight(0.05)
+    plt.show()
