a6c9a2982ef72ab9d817ee87b8f98fee7d0ffdca
Sergio Martin <martiser@ethz.ch>
2019-11-07 15:52:56 +0100

    Adding problem/solver pairing

--- source/korali.hpp
+++ source/korali.hpp
@@ -1,3 +1,23 @@
 #include "engine/engine.hpp"
 #include "auxiliar/py2json.hpp"
 #include "conduit/MPI/MPI.hpp"
+#include "conduit/conduit.hpp"
+
+namespace korali
+{
+ // Main Thread
+ size_t _engineCount;
+
+ // Conduit
+ extern korali::Conduit* _conduit;
+
+ // State save/load methods
+ void saveProfilingInfo();
+ void run(std::vector<korali::Engine> engines);
+
+ // Run configuration
+ nlohmann::json _js;
+
+ // Communicator Methods
+ long int getMPICommPointer();
+}
