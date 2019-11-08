101c14020dfe33a2c858c9aac5bf9f751e238991
wadaniel <wadaniel@ethz.ch>
2019-11-08 08:36:48 +0100

    renamed scripts in tut 7a

--- tutorials/a7-simple-hierarchical-bayesian/run-hierarchical.sh
+++ tutorials/a7-simple-hierarchical-bayesian/run-hierarchical.sh
@@ -4,7 +4,7 @@
 set -e
 
 cd phase0
-python3 x.py
+python3 phase0.py
 cd ../
 
 cd phase1
@@ -12,13 +12,13 @@ sh generateData.sh
 cd ../
 
 cd phase2
-python3 x.py
+python3 phase2.py
 cd ../
 
 cd phase3a
-python3 x.py
+python3 phase3a.py
 cd ../
 
 cd phase3b
-python3 x.py
+python3 phase3b.py
 cd ../
