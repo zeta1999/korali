1f167c8b43ba5f26c47a929b5ff2bdd43f90c75a
wadaniel <wadaniel@ethz.ch>
2019-11-12 10:28:41 +0100

    STAT001

--- tests/STAT-001/run-dram.py
+++ tests/STAT-001/run-dram.py
@@ -21,11 +21,12 @@ k["Solver"]["Type"] = "Sampler/MCMC"
 k["Solver"]["Burn In"] = 500
 k["Solver"]["Termination Criteria"]["Max Samples"] = 5000
 k["Solver"]["Rejection Levels"] = 2
+k["Solver"]["Use Adaptive Sampling"] = True
 
 # Defining problem's variables and their MCMC settings
 k["Variables"][0]["Name"] = "X"
 k["Variables"][0]["Initial Mean"] = 1.0
-k["Variables"][0]["Initial Standard Deviation"] = 1.0
+k["Variables"][0]["Initial Standard Deviation"] = 2.0
 
 # Running Korali
 k["Random Seed"] = 0xC0FFEE
