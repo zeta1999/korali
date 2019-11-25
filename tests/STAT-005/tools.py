893a5fe8f91639a9c32f741c34cfe0249c6c42af
Georgios Arampatzis <garampat@ethz.ch>
2019-11-25 23:00:34 +0100

    STATS-005 does not pass in circleci and panda

--- tests/STAT-005/tools.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import os, glob
-
-def find_last_generation( dir ):
-  lst = []
-  for file in glob.glob(dir+"/gen*.json"):
-    lst.append(file)
-
-  return sorted(lst)[-1]
--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -4,13 +4,22 @@ source functions.sh
 
 #Running tutorials
 
-for dir in */
-do
-  echo "[Korali] Running test in folder ${dir} ..." 
-  pushd $dir
-  ./run_test.sh
-  if [ ! $? -eq 0 ]; then exit -1; fi
-  popd 
-done
+dir=STAT-005
+echo "[Korali] Running test in folder ${dir} ..."
+pushd $dir
+./run_test.sh
+if [ ! $? -eq 0 ]; then exit -1; fi
+popd
 
 echo "[Korali] All tests ran correctly."
+
+# for dir in */
+# do
+#   echo "[Korali] Running test in folder ${dir} ..."
+#   pushd $dir
+#   ./run_test.sh
+#   if [ ! $? -eq 0 ]; then exit -1; fi
+#   popd
+# done
+#
+# echo "[Korali] All tests ran correctly."
