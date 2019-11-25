72c191352658a5ceeffb4fc06dd1320db84e1a14
Georgios Arampatzis <garampat@ethz.ch>
2019-11-25 23:07:24 +0100

    correct run_all_tests.sh

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -4,22 +4,13 @@ source functions.sh
 
 #Running tutorials
 
-dir=STAT-005
-echo "[Korali] Running test in folder ${dir} ..."
-pushd $dir
-./run_test.sh
-if [ ! $? -eq 0 ]; then exit -1; fi
-popd
+for dir in */
+do
+  echo "[Korali] Running test in folder ${dir} ..."
+  pushd $dir
+  ./run_test.sh
+  if [ ! $? -eq 0 ]; then exit -1; fi
+  popd
+done
 
 echo "[Korali] All tests ran correctly."
-
-# for dir in */
-# do
-#   echo "[Korali] Running test in folder ${dir} ..."
-#   pushd $dir
-#   ./run_test.sh
-#   if [ ! $? -eq 0 ]; then exit -1; fi
-#   popd
-# done
-#
-# echo "[Korali] All tests ran correctly."
