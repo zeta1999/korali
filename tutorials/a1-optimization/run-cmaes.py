a80adb870ea10e437ae797eff647120079cb04de
wadaniel <wadaniel@ethz.ch>
2019-11-27 12:30:13 +0100

    allowing 0 console frequency

--- tutorials/a1-optimization/run-cmaes.py
+++ tutorials/a1-optimization/run-cmaes.py
@@ -32,5 +32,6 @@ e["Solver"]["Population Size"] = 32
 e["Solver"]["Termination Criteria"]["Min Value Difference Threshold"] = 1e-7
 e["Solver"]["Termination Criteria"]["Max Generations"] = 100
 
+e["Console"]["Frequency"] = 0
 # Running Korali
 k.run(e)
