ac45fe21fd20a25fb418435163090dea3c5dac60
wadaniel <wadaniel@ethz.ch>
2019-11-29 11:46:44 +0100

    fixed minimizing issue

---
+++ tests/REG-007/run-maxcmaes1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append('./model')
+from model import *
+
+import korali
+k = korali.Engine()
+
+e = korali.Experiment()
+
+e["Problem"]["Type"] = "Evaluation/Direct/Basic"
+e["Problem"]["Objective"] = "Maximize"
+e["Problem"]["Objective Function"] = maxmodel1
+
+e["Variables"][0]["Name"] = "X"
+e["Variables"][0]["Lower Bound"] = -10.0
+e["Variables"][0]["Upper Bound"] = +10.0
+
+e["Solver"]["Type"] = "Optimizer/CMAES"
+e["Solver"]["Population Size"] = 32
+e["Solver"]["Termination Criteria"]["Min Value Difference Threshold"] = 1e-8
+e["Solver"]["Termination Criteria"]["Max Generations"] = 100
+
+k.run(e)
+
+xopt = e["Solver"]["Internal"]["Best Ever Variables"]
+fopt = e["Solver"]["Internal"]["Best Ever Value"]
+
+x = e["Solver"]["Internal"]["Current Best Variables"]
+f = e["Solver"]["Internal"]["Current Best Value"]
+
+assertclose(2.0, xopt, 1e-4)
+assertclose(2.0, x, 1e-3)
+assertclose(-10.0, fopt, 1e-6)
+assertclose(-10.0, f, 1e-4)
---
+++ tests/REG-007/run_test.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+source ../functions.sh
+
+
+#################################################
+# MIN/MAX Optimizer
+#################################################
+
+logEcho "[Korali] Beginning minimizing mazimizing tests"
+
+logEcho "-------------------------------------"
+logEcho "Testing Maximizing"
+logEcho "Running File: run-maxcmaes1.py"
+
+python3 ./run-maxcmaes1.py >> $logFile 2>&1
+check_result
+
+log "[Korali] Removing results..."
+rm -rf "_korali_result" >> $logFile 2>&1
+check_result
+
+logEcho "-------------------------------------"
+
+logEcho "-------------------------------------"
+logEcho "Testing Maximizing"
+logEcho "Running File: run-maxcmaes2.py"
+
+python3 ./run-maxcmaes2.py >> $logFile 2>&1
+check_result
+
+log "[Korali] Removing results..."
+rm -rf "_korali_result" >> $logFile 2>&1
+check_result
+
+logEcho "-------------------------------------"
+
+logEcho "-------------------------------------"
+logEcho "Testing Minimizing"
+logEcho "Running File: run-mincmaes1.py"
+
+python3 ./run-mincmaes1.py >> $logFile 2>&1
+check_result
+
+log "[Korali] Removing results..."
+rm -rf "_korali_result" >> $logFile 2>&1
+check_result
+
+logEcho "-------------------------------------"
+
+logEcho "-------------------------------------"
+logEcho "Testing Minimizing"
+logEcho "Running File: run-mincmaes2.py"
+
+python3 ./run-mincmaes2.py >> $logFile 2>&1
+check_result
+
+log "[Korali] Removing results..."
+rm -rf "_korali_result" >> $logFile 2>&1
+check_result
+
+logEcho "-------------------------------------"
+
