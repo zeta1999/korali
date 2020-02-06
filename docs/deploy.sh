d18a48859bc567a8ef93c341f671ce77d01e2338
Sergio Martin <martiser@ethz.ch>
2020-02-06 15:56:00 +0100

    Updating deploy script

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -17,16 +17,8 @@ check
 
 # Copying from falcon to vladimirovich (host)
 
-git branch | grep \* | grep master
-if [ $? -eq 0 ] 
-then
-  echo "[Korali] Deploying to master webpage."
-  ssh circleci@falcon.ethz.ch './update_master.sh'
-  check
-else
-  echo "[Korali] Deploying to development webpage."
-  ssh circleci@falcon.ethz.ch './update_development.sh'
-  check
-fi
+echo "[Korali] Deploying to webpage."
+ssh circleci@falcon.ethz.ch './update.sh'
+check
 
 echo "[Korali] Deploying complete."
