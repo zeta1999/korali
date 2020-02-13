d28c836b819ff1f6587e53ea3fc4657a4cfb0508
Sergio Martin <martiser@ethz.ch>
2020-02-13 11:31:59 +0100

    Creating read the docs documentation

--- docs/build.sh
+++ docs/build.sh
@@ -9,23 +9,20 @@ function check()
  fi 
 }
 
-pushd manual
 
-# Build pages from source code
-#pushd builder
-#python3 ./buildWeb.py
-#check
 
-popd
+# Installing shpinx, mkdocs, and materials theme
+python3 -m pip install sphinx --user
+check
 
-pushd website
-
-# Installing mkdocs and materials theme
 python3 -m pip install mkdocs --user
 check
 
 python3 -m pip install mkdocs-material --user
 check
+
+# Building main website
+pushd website
 
 python3 -m mkdocs build
 check
@@ -35,5 +32,18 @@ sed -i -e 's/76\./36\./g;s/12\.1/10\.5/g' ./site/assets/stylesheets/application.
 check
 
 popd 
+
+# Building User Manual
+pushd manual
+
+make html
+check
+
+popd
+
+# Inserting user manual into website
+
+cp -r manual/.build/html website/site/docs
+check
 
 echo "[Korali] Webpage Build complete."
---
+++ docs/manual/index.rst
@@ -0,0 +1,20 @@
+.. Korali documentation master file, created by
+   sphinx-quickstart on Thu Feb 13 11:19:39 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Korali's documentation!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
