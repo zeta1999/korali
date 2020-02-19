e06d572d4bdb81e22c4762102258d44a072e581c
Sergio Martin <martiser@ethz.ch>
2020-02-19 16:09:47 +0100

    Updating docs

--- docs/manual/builder/buildModules.py
+++ docs/manual/builder/buildModules.py
@@ -5,6 +5,8 @@ import os
 import json
 import shutil
 
+moduleSrcDir = '../../../source/modules'
+
 def getVariableName(v):
  cVarName = v["Name"].replace(" ", "")
  cVarName = '_' + cVarName[0].lower() + cVarName[1:]
@@ -20,8 +22,8 @@ def getJsonPath(path):
   nameString += '\\["' + item + '"\\]'
  return nameString
  
-def processLeafModuleSubFolder(root, moduleName):
- modulePath = os.path.join(root, moduleName)
+def processLeafModuleSubFolder(moduleRelPath, moduleName):
+ modulePath = os.path.join(moduleSrcDir, moduleRelPath)
  moduleConfigFile = modulePath + '/' + moduleName + '.config'
  moduleReadmeFile = modulePath + '/README.rst'
  
@@ -41,24 +43,26 @@ def processLeafModuleSubFolder(root, moduleName):
    moduleReadmeString += ' - **Description**: ' + v["Description"] + '\n'
    moduleReadmeString +='\n'  
     
- moduleOutputDir = '../modules/' + root
+ moduleOutputDir = '../modules/' + moduleRelPath
+ if not os.path.exists(moduleOutputDir):
+   os.mkdir(moduleOutputDir) 
+  
  with open(moduleOutputDir + '/' + moduleName + '.rst', 'w') as file: file.write(moduleReadmeString)
  
-def processParentModuleSubFolder(root, moduleName):
- modulePath = os.path.join(root, moduleName)
- 
+def processParentModuleSubFolder(moduleRelPath, moduleName):
+ modulePath = os.path.join(moduleSrcDir, moduleRelPath)
  moduleReadmeFile = modulePath + '/README.rst'
  with open(moduleReadmeFile, 'r') as file: moduleReadmeString = file.read()
 
- #moduleReadmeString += '  .. toctree::\n'
- #moduleReadmeString += '     :maxdepth: 1\n'
- #moduleReadmeString += '     :caption: Sub-Categories\n'
+ moduleReadmeString += '  .. toctree::\n'
+ moduleReadmeString += '     :maxdepth: 1\n'
+ moduleReadmeString += '     :caption: Sub-Categories\n' 
 
- moduleOutputDir = '../modules/' + root
+ moduleOutputDir = '../modules/' + moduleRelPath
+ print('Creating: ' + moduleOutputDir)
  if not os.path.exists(moduleOutputDir):
   os.mkdir(moduleOutputDir) 
- with open(moduleOutputDir + '/' + moduleName + '.rst', 'w') as file: file.write(moduleReadmeString)
-     
+      
  list_dir = os.listdir(modulePath)
  for f in list_dir:
   subModuleFullPath = os.path.join(modulePath, f)
@@ -66,16 +70,19 @@ def processParentModuleSubFolder(root, moduleName):
    subModuleConfigFile = subModuleFullPath + '/' + f + '.config'
    with open(subModuleConfigFile, 'r') as file: subModuleConfigString = file.read()
    subModuleConfig = json.loads(subModuleConfigString)
-   moduleReadmeString += ' ' + f + ' \n'
-
+   moduleReadmeString += '    ' + subModuleFullPath + '/' + f + ' \n'
+ 
    subModuleDstPath = moduleOutputDir + '/' + f
    if not os.path.exists(subModuleDstPath):
      os.mkdir(subModuleDstPath) 
-  
+
+ moduleReadmeString += '\n\n'
+ with open(moduleOutputDir + '/' + moduleName + '.rst', 'w') as file: file.write(moduleReadmeString)
+    
 shutil.rmtree('../modules', ignore_errors=True, onerror=None)
 os.makedirs('../modules')
 
-for root, dirs, files in os.walk("../../../source/modules", topdown=True):
+for root, dirs, files in os.walk(moduleSrcDir, topdown=True):
  for name in dirs:
   fullPath = os.path.join(root, name)
   if (not '.o/' in fullPath and not '.d/' in fullPath):
@@ -84,12 +91,14 @@ for root, dirs, files in os.walk("../../../source/modules", topdown=True):
    for f in list_dir:
     if not os.path.isfile(os.path.join(fullPath, f)):
      hasSubFolders = True
+   relPath = os.path.relpath(fullPath, moduleSrcDir)
+   print('Comparing: ' + fullPath + ' to ' + moduleSrcDir + ' - Got: ' + relPath)
    print(name)
    if (hasSubFolders):
      print('Has Subfolders')
-     processParentModuleSubFolder(root, name)
+     processParentModuleSubFolder(relPath, name)
    else:
      print('Has No Subfolders')
-     processLeafModuleSubFolder(root, name)
+     processLeafModuleSubFolder(relPath, name)
      
      
