daeffb31afa06091460ea63e650a246b42e89727
Sergio Martin <martiser@ethz.ch>
2020-02-19 10:54:44 +0100

    Updated docs

--- docs/manual/builder/buildModules.py
+++ docs/manual/builder/buildModules.py
@@ -4,8 +4,38 @@ import sys
 import os
 import json
 import shutil
-from buildAux import *
 
+def getVariableName(v):
+ cVarName = v["Name"].replace(" ", "")
+ cVarName = '_' + cVarName[0].lower() + cVarName[1:]
+ return cVarName
+
+def getDataType(v):
+ cVarType = v["Type"].replace('size_t', 'Unsigned Integer').replace('int', 'Integer').replace('double', 'Real Number').replace('std::string', 'String').replace('std::vector<', 'Array of ').replace('<','')
+ return cVarType 
+ 
+def getJsonPath(path):
+ nameString = ''
+ for item in path:
+  nameString += '["' + item + '"]'
+ return nameString
+ 
+def getVariableInfo(v, moduleName):
+ varString = ''
+ varString += '??? abstract "' + getJsonPath(v["Name"]) + '"\n\n'
+ varString += '\t' + v["Description"] + '\n'
+ varString += '\n'
+ 
+ if ('Korali::Distribution' in v["Type"]):
+  with open('distributionList.txt', 'r') as file: distributionListString = file.read()
+  varString += '\t**Available Distributions**: \n'
+  varString += distributionListString
+  return varString
+  
+ varString += '\t+ Type: ' + getDataType(v) + '\n'
+ varString += '\t+ Description: v["Description"]\n\n' 
+ return varString
+ 
 def processModuleSubFolder(modulePath):
  moduleDir = '../../../source/' + modulePath
  moduleName = os.path.basename(os.path.normpath(modulePath))
