eda4ebca686901c9ba2fb4d9bb38f49e9c01c780
Sergio Martin <martiser@ethz.ch>
2020-02-19 12:08:16 +0100

    Updating web/docs

--- docs/manual/builder/buildModules.py
+++ docs/manual/builder/buildModules.py
@@ -11,32 +11,16 @@ def getVariableName(v):
  return cVarName
 
 def getDataType(v):
- cVarType = v["Type"].replace('size_t', 'Unsigned Integer').replace('int', 'Integer').replace('double', 'Real Number').replace('std::string', 'String').replace('std::vector<', 'Array of ').replace('<','')
+ cVarType = v["Type"].replace('size_t', 'Unsigned Integer').replace('int', 'Integer').replace('double', 'Real Number').replace('std::string', 'String').replace('std::vector<std::vector<', 'List of Lists of ').replace('std::vector<', 'List of ').replace('>','').replace('bool', 'True/False')
  return cVarType 
  
 def getJsonPath(path):
  nameString = ''
  for item in path:
-  nameString += '["' + item + '"]'
+  nameString += '\\["' + item + '"\\]'
  return nameString
  
-def getVariableInfo(v, moduleName):
- varString = ''
- varString += '??? abstract "' + getJsonPath(v["Name"]) + '"\n\n'
- varString += '\t' + v["Description"] + '\n'
- varString += '\n'
- 
- if ('Korali::Distribution' in v["Type"]):
-  with open('distributionList.txt', 'r') as file: distributionListString = file.read()
-  varString += '\t**Available Distributions**: \n'
-  varString += distributionListString
-  return varString
-  
- varString += '\t+ Type: ' + getDataType(v) + '\n'
- varString += '\t+ Description: v["Description"]\n\n' 
- return varString
- 
-def processModuleSubFolder(modulePath):
+def processLeafModuleSubFolder(modulePath):
  moduleDir = '../../../source/' + modulePath
  moduleName = os.path.basename(os.path.normpath(modulePath))
  
@@ -48,22 +32,63 @@ def processModuleSubFolder(modulePath):
 
  moduleConfig = json.loads(moduleConfigString)
  
- moduleReadmeString += '\n\nModule Configuration\n'
+ moduleReadmeString += '\nConfiguration\n'
  moduleReadmeString += '#####################\n\n'
  
  if ('Configuration Settings' in moduleConfig):
   for v in moduleConfig["Configuration Settings"]:
-   moduleReadmeString += getVariableInfo(v, moduleName)
-  moduleReadmeString +='\n\n'  
+   moduleReadmeString += '\n'
+   moduleReadmeString += getJsonPath(v["Name"]) + '\n'
+   moduleReadmeString += ' - **Type**: ' + getDataType(v) + '\n'
+   moduleReadmeString += ' - **Description**: ' + v["Description"] + '\n'
+   moduleReadmeString +='\n'  
     
  moduleOutputDir = '../modules/'
  with open(moduleOutputDir + '/' + moduleName + '.rst', 'w') as file: file.write(moduleReadmeString)
  
+def processParentModuleSubFolder(modulePath):
+ moduleDir = '../../../source/' + modulePath
+ moduleName = os.path.basename(os.path.normpath(modulePath))
+ 
+ moduleReadmeFile = moduleDir + '/README.rst'
+ with open(moduleReadmeFile, 'r') as file: moduleReadmeString = file.read()
+
+ #moduleReadmeString += '  .. toctree::\n'
+ #moduleReadmeString += '     :maxdepth: 1\n'
+ #moduleReadmeString += '     :caption: Sub-Categories\n'
+    
+ list_dir = os.listdir(moduleDir)
+ for f in list_dir:
+  subModuleFullPath = os.path.join(moduleDir, f)
+  if not os.path.isfile(subModuleFullPath):
+   subModuleConfigFile = subModuleFullPath + '/' + f + '.config'
+   with open(subModuleConfigFile, 'r') as file: subModuleConfigString = file.read()
+   subModuleConfig = json.loads(subModuleConfigString)
+   moduleReadmeString += ' ' + f + ' \n' 
+ 
+ moduleOutputDir = '../modules/'
+ with open(moduleOutputDir + '/' + moduleName + '.rst', 'w') as file: file.write(moduleReadmeString)
+  
 shutil.rmtree('../modules', ignore_errors=True, onerror=None)
 os.makedirs('../modules')
 
-processModuleSubFolder('engine')
-processModuleSubFolder('experiment')
-processModuleSubFolder('solver')
-processModuleSubFolder('problem')
-processModuleSubFolder('conduit')
+processLeafModuleSubFolder('engine')
+processLeafModuleSubFolder('experiment')
+processParentModuleSubFolder('solver')
+processParentModuleSubFolder('problem')
+processParentModuleSubFolder('conduit')
+
+for root, dirs, files in os.walk("../../../source/solver", topdown=True):
+ for name in dirs:
+  fullPath = os.path.join(root, name)
+  if (not '.o/' in fullPath and not '.d/' in fullPath):
+   list_dir = os.listdir(fullPath)
+   hasSubFolders = False
+   for f in list_dir:
+    if not os.path.isfile(os.path.join(fullPath, f)):
+     hasSubFolders = True
+   print(fullPath)
+   #if (hasSubFolders):
+   #  print('Has Subfolders')
+   #else:
+   #  print('Has No Subfolders')
