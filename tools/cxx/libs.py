e229cf12cfa0e0bf41b40891a29f64b8cfa9116b
Fabian Wermelinger <fabianw@mavt.ethz.ch>
2020-04-10 17:59:45 +0200

    Fix linker -Wl option; add python library for linker
    The combination
    
    -Wl,-rpath -Wl,<path>
    
    does not work correctly when using korali as a library where linker
    flags are obtained with
    
    python3 -m korali.cxx --libs
    
    CMake applies permutations to the string and violates the sequential
    order requirement of the linker options above.  The options are changed
    to:
    
    -Wl,-rpath,<path>
    
    The -lpython3 library is also missing at link time (this assumes the
    environment is setup correctly and the linker is able to find the right
    python library to link against)

--- tools/cxx/libs.py
+++ tools/cxx/libs.py
@@ -3,7 +3,8 @@
 def getlibs(extdir, makeFlags):
  flags=''
  pythonLibPaths=[ p for p in makeFlags['PYBIND11LIBS'].split() if p.startswith('-L') ]
- for p in pythonLibPaths: flags += ' -Wl,-rpath,' + p.replace('-L', '') + ' '  
- flags+='-L' + extdir + ' -lkorali -Wl,-rpath -Wl,' + extdir + ' '
- flags+='-L' + makeFlags['GSLPREFIX'] + '/lib -Wl,-rpath -Wl,' + makeFlags['GSLPREFIX'] + '/lib ' 
+ for p in pythonLibPaths: flags += ' -Wl,-rpath,' + p.replace('-L', '') + ' '
+ flags+='-L' + extdir + ' -lkorali -Wl,-rpath,' + extdir + ' '
+ flags+='-L' + makeFlags['GSLPREFIX'] + '/lib -Wl,-rpath,' + makeFlags['GSLPREFIX'] + '/lib '
+ flags+='-lpython3'
  print(flags + ' ' + makeFlags['GSLLIBS'] + ' ' + makeFlags['PYBIND11LIBS'])
