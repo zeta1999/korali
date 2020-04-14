703b8d82a28c2be848fa747ea1773bb0bc138464
Pascal Weber <webepasc@student.ethz.ch>
2020-04-14 10:36:52 +0200

    bugfix for MC integration

--- tutorials/basic/11.integration/model/integrand.py
+++ tutorials/basic/11.integration/model/integrand.py
@@ -5,6 +5,8 @@ import numpy as np
 
 def integrand( sample ):
   x = sample["Parameters"][0] 
-  sample["Evaluation"] = x**2
-  #y = sample["Parameters"][1] 
-  #sample["Evaluation"] = x**2+y**2 
+  #sample["Evaluation"] = x**2
+  y = sample["Parameters"][1] 
+  #sample["Evaluation"] = x**2+y**2
+  z = sample["Parameters"][2]
+  sample["Evaluation"] = x**2+y**2+z**2
--- tutorials/basic/11.integration/run-integration.py
+++ tutorials/basic/11.integration/run-integration.py
@@ -15,15 +15,33 @@ e = korali.Experiment()
 
 e["Problem"]["Type"] = "Integration"
 e["Problem"]["Integrand"] = lambda modelData: integrand(modelData)
-e["Problem"]["Integration Method"] = "Simpson"#"Rectangle" #"Trapezoidal", "Simpson", "MonteCarlo", "Custom"
+e["Problem"]["Integration Method"] = "Monte Carlo"#"Rectangle" #"Trapezoidal", "Simpson", "Monte Carlo", "Custom"
+e["Problem"]["Number Of Samples"] = 1000
+
+e["Distributions"][0]["Name"] = "Uniform"
+e["Distributions"][0]["Type"] = "Univariate/Uniform"
+e["Distributions"][0]["Minimum"] = 0.0
+e["Distributions"][0]["Maximum"] = +1.0
+
 e["Variables"][0]["Name"] = "x"
+e["Variables"][0]["Number Of Gridpoints"] = 10
 e["Variables"][0]["Lower Bound"] = 0.0
-e["Variables"][0]["Upper Bound"] = 5.0
-e["Variables"][0]["Number Of Gridpoints"] = 100
-#e["Variables"][1]["Name"] = "y"
-#e["Variables"][1]["Lower Bound"] = 0.0
-#e["Variables"][1]["Upper Bound"] = 5.0
-#e["Variables"][1]["Number Of Gridpoints"] = 100
+e["Variables"][0]["Upper Bound"] = 1.0
+e["Variables"][0]["Prior Distribution"] = "Uniform" 
+
+e["Variables"][1]["Name"] = "y"
+e["Variables"][1]["Lower Bound"] = 0.0
+e["Variables"][1]["Upper Bound"] = 1.0
+e["Variables"][1]["Number Of Gridpoints"] = 10
+e["Variables"][1]["Prior Distribution"] = "Uniform" 
+
+e["Variables"][2]["Name"] = "z"
+e["Variables"][2]["Lower Bound"] = 0.0
+e["Variables"][2]["Upper Bound"] = 1.0
+e["Variables"][2]["Number Of Gridpoints"] = 10
+e["Variables"][2]["Prior Distribution"] = "Uniform" 
+
+
 e["Solver"]["Type"] = "Integrator"
 #e["Solver"]["Executions Per Generation"] = 0
 e["Console Output"]["Verbosity"] = "Detailed"
