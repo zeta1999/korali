5807db5eb9c8d9cc00413576675cb2308f4a8e86
Pascal Weber <webepasc@student.ethz.ch>
2020-04-14 11:57:22 +0200

    bugfix

--- tutorials/basic/11.integration/run-integration.py
+++ tutorials/basic/11.integration/run-integration.py
@@ -15,8 +15,7 @@ e = korali.Experiment()
 
 e["Problem"]["Type"] = "Integration"
 e["Problem"]["Integrand"] = lambda modelData: integrand(modelData)
-e["Problem"]["Integration Method"] = "Monte Carlo"#"Rectangle" #"Trapezoidal", "Simpson", "Monte Carlo", "Custom"
-e["Problem"]["Number Of Samples"] = 1000
+e["Problem"]["Integration Method"] = "Rectangle"#"Rectangle" #"Trapezoidal", "Simpson", "Monte Carlo", "Custom"
 
 e["Distributions"][0]["Name"] = "Uniform"
 e["Distributions"][0]["Type"] = "Univariate/Uniform"
@@ -24,22 +23,22 @@ e["Distributions"][0]["Minimum"] = 0.0
 e["Distributions"][0]["Maximum"] = +1.0
 
 e["Variables"][0]["Name"] = "x"
-e["Variables"][0]["Number Of Gridpoints"] = 10
+e["Variables"][0]["Number Of Gridpoints"] = 100
 e["Variables"][0]["Lower Bound"] = 0.0
 e["Variables"][0]["Upper Bound"] = 1.0
-e["Variables"][0]["Prior Distribution"] = "Uniform" 
+e["Variables"][0]["Sampling Distribution"] = "Uniform" 
 
 e["Variables"][1]["Name"] = "y"
 e["Variables"][1]["Lower Bound"] = 0.0
 e["Variables"][1]["Upper Bound"] = 1.0
-e["Variables"][1]["Number Of Gridpoints"] = 10
-e["Variables"][1]["Prior Distribution"] = "Uniform" 
+e["Variables"][1]["Number Of Gridpoints"] = 100
+e["Variables"][1]["Sampling Distribution"] = "Uniform" 
 
 e["Variables"][2]["Name"] = "z"
 e["Variables"][2]["Lower Bound"] = 0.0
 e["Variables"][2]["Upper Bound"] = 1.0
-e["Variables"][2]["Number Of Gridpoints"] = 10
-e["Variables"][2]["Prior Distribution"] = "Uniform" 
+e["Variables"][2]["Number Of Gridpoints"] = 100
+e["Variables"][2]["Sampling Distribution"] = "Uniform" 
 
 
 e["Solver"]["Type"] = "Integrator"
