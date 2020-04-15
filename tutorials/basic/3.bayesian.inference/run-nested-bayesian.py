d94e884ae78b11fa9597a48b11c9fb07de305140
wadaniel <wadaniel@ethz.ch>
2020-04-15 14:45:45 +0200

    fix ellipse

--- tutorials/basic/3.bayesian.inference/run-nested-bayesian.py
+++ tutorials/basic/3.bayesian.inference/run-nested-bayesian.py
@@ -25,7 +25,7 @@ e["Solver"]["Number Live Points"] = 1500
 e["Solver"]["Batch Size"] = 1
 e["Solver"]["Covariance Scaling"] = 1.0
 e["Solver"]["Add Live Points"] = True
-e["Solver"]["Resampling Method"] = "Box"
+e["Solver"]["Resampling Method"] = "Ellipse"
 
 
 # Configuring the problem's random distributions
