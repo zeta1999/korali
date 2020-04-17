f9ca8cef94dd90c959c18d595e6aa68c9c1df442
Sergio Martin <martiser@ethz.ch>
2020-04-17 17:23:55 +0200

    Starting RL branch

--- tools/mdev/solver/base.cpp
+++ tools/mdev/solver/base.cpp
@@ -10,26 +10,27 @@ void korali::solver::[CLASSNAME]::runGeneration()
 {
  if (_k->_currentGeneration == 1) setInitialConfiguration();
 
- korali::logInfo("Normal", "Running generation %lu...\n", _k->_currentGeneration);
+ _k->_logger->logInfo("Normal", "Running generation %lu...\n", _k->_currentGeneration);
 
  // Example: Evaluating a sample
  korali::Sample sample;
+ sample["Module"] = "Problem";
  sample["Operation"] = "Evaluate";
  korali::_conduit->start(sample);
  korali::_conduit->wait(sample);
 
  double evaluation = sample["Evaluation"];
- korali::logInfo("Normal", "Sample Evaluation: %f\n", evaluation);
+ _k->_logger->logInfo("Normal", "Sample Evaluation: %f\n", evaluation);
 
 }
 
 void korali::solver::[CLASSNAME]::printGenerationBefore()
 {
- korali::logInfo("Normal", "Preparing to start generation...\n");
+ _k->_logger->logInfo("Normal", "Preparing to start generation...\n");
 }
 
 void korali::solver::[CLASSNAME]::printGenerationAfter()
 {
- korali::logInfo("Normal", "Finished to generation %lu...\n", _k->_currentGeneration);
+ _k->_logger->logInfo("Normal", "Finished to generation %lu...\n", _k->_currentGeneration);
 }
 
