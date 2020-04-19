5778bbd77187b13ec3db043300fe1412880187e9
webepasc <webepasc@student.ethz.ch>
2020-04-19 15:44:55 +0200

    cheaper integration problem

--- tutorials/basic/11.integration/run-integration.py
+++ tutorials/basic/11.integration/run-integration.py
@@ -23,7 +23,7 @@ e["Distributions"][0]["Minimum"] = 0.0
 e["Distributions"][0]["Maximum"] = +1.0
 
 e["Variables"][0]["Name"] = "x"
-e["Variables"][0]["Number Of Gridpoints"] = 100
+e["Variables"][0]["Number Of Gridpoints"] = 10
 e["Variables"][0]["Lower Bound"] = 0.0
 e["Variables"][0]["Upper Bound"] = 1.0
 e["Variables"][0]["Sampling Distribution"] = "Uniform" 
@@ -31,13 +31,13 @@ e["Variables"][0]["Sampling Distribution"] = "Uniform"
 e["Variables"][1]["Name"] = "y"
 e["Variables"][1]["Lower Bound"] = 0.0
 e["Variables"][1]["Upper Bound"] = 1.0
-e["Variables"][1]["Number Of Gridpoints"] = 100
+e["Variables"][1]["Number Of Gridpoints"] = 10
 e["Variables"][1]["Sampling Distribution"] = "Uniform" 
 
 e["Variables"][2]["Name"] = "z"
 e["Variables"][2]["Lower Bound"] = 0.0
 e["Variables"][2]["Upper Bound"] = 1.0
-e["Variables"][2]["Number Of Gridpoints"] = 100
+e["Variables"][2]["Number Of Gridpoints"] = 10
 e["Variables"][2]["Sampling Distribution"] = "Uniform" 
 
 
