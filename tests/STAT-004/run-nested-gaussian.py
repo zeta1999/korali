0c646595e90de7763780fc628b16f3a6a02e0eeb
wadaniel <wadaniel@ethz.ch>
2020-04-20 12:24:10 +0200

    statistical tests, cleanup

--- tests/STAT-004/run-nested-gaussian.py
+++ tests/STAT-004/run-nested-gaussian.py
@@ -34,7 +34,7 @@ e["Variables"][0]["Name"] = "a"
 e["Variables"][0]["Prior Distribution"] = "Uniform 0"
 
 e["File Output"]["Frequency"] = 0
-e["Console Output"]["Frequency"] = 100
+e["Console Output"]["Frequency"] = 1000
 e["Solver"]["Termination Criteria"]["Max Generations"] = 20000
 e["Solver"]["Termination Criteria"]["Max Gain Factor"] = 1e-9
 e["Random Seed"] = 1227
@@ -43,5 +43,5 @@ e["Random Seed"] = 1227
 k = korali.Engine()
 k.run(e)
 
-verifyMean(e["Results"]["Posterior Samples"], [-2.0], 0.05)
-verifyStd(e["Results"]["Posterior Samples"], [3.0], 0.05)
+verifyMean(e["Results"]["Posterior Samples"], [-2.0], 0.1)
+verifyStd(e["Results"]["Posterior Samples"], [3.0], 0.1)
