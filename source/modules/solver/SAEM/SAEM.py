53e20fe536f5fcd7ff578ad29addb8e8c230e859
Susi <susanne.keller@protonmail.com>
2020-04-22 22:39:16 +0200

    add sub-titles to both colums

--- source/modules/solver/SAEM/SAEM.py
+++ source/modules/solver/SAEM/SAEM.py
@@ -42,22 +42,50 @@ def plotVariables(genList):
         hyperparams[idx, ...] = genList[i]["Solver"]["Current Hyperparameters"]
         gammas[idx] = genList[i]["Solver"]["Gamma"]
 
-    fig, ax = plt.subplots(nrows=max(numLatent, numHyper) + 1, ncols=2, figsize=(8, 20), dpi=60)
+    # Create an "outer" subplot for the two column titles
+    fig, ax0 = plt.subplots(nrows=1, ncols=2, figsize=(8, 20), dpi=60)
+    ax0[0].set_title("Latent Variables",  y=1.03, fontdict={'fontweight': 8, 'fontsize': 20})
+    ax0[1].set_title("Hyperparameters",  y=1.03, fontdict={'fontweight': 8, 'fontsize': 20})
+
+    # Make outer plot invisible
+    ax0[0].tick_params(labelcolor=(1.,1.,1., 0.0), top='off', bottom='off', left='off', right='off')
+    ax0[0]._frameon = False
+    ax0[1].tick_params(labelcolor=(1.,1.,1., 0.0), top='off', bottom='off', left='off', right='off')
+    ax0[1]._frameon = False
+    ax0[0].tick_params(
+        axis='both',  # changes apply to the x-axis
+        which='both',  # both major and minor ticks are affected
+        bottom=False,  # ticks along the bottom edge are off
+        top=False,  # ticks along the top edge are off
+        labelbottom=False)
+    ax0[1].tick_params(
+        axis='both',
+        which='both',
+        bottom=False,
+        top=False,
+        labelbottom=False)
+
+
     fig.subplots_adjust(hspace=.8)
-    # samplesTmp = np.reshape( samples, (numentries,numdim) )
-    plt.suptitle('SAEM Plotter - \nNumber of Samples {0}\n'.format(str(numSamples)),
-                 fontweight='bold', fontsize=12)
+
+    # suptitle?
+    plt.suptitle('SAEM Plotter - \nNumber of Samples: {0}\n'.format(str(numSamples)),
+                 fontweight='bold', fontsize=20)
     for latentIdx in range(numLatent):
-        ax[latentIdx, 0].errorbar(x=generations, y=sample_means[:, latentIdx], yerr=sample_sdevs[:, latentIdx])
-        ax[latentIdx, 0].set_title(latentVars[latentIdx]["Name"])
+        ax = fig.add_subplot(max(numLatent, numHyper) + 1, 2, 2 * latentIdx + 1)
+        ax.errorbar(x=generations, y=sample_means[:, latentIdx], yerr=sample_sdevs[:, latentIdx])
+        ax.set_title(latentVars[latentIdx]["Name"])
     # fig, ax = plt.subplots(nrows=max(numLatent, numHyper) + 1, ncols=1, figsize=(15,8))
     for hyperIdx in range(numHyper):
-        ax[hyperIdx, 1].plot(generations, hyperparams[:, hyperIdx])
-        ax[hyperIdx, 1].set_title(hyperVars[hyperIdx]["Name"])
-    ax[-1, 0].plot(generations, gammas)
-    ax[-1, 0].set_title("gamma")
+        ax = fig.add_subplot(max(numLatent, numHyper) + 1, 2, 2 * hyperIdx + 2)
+        ax.plot(generations, hyperparams[:, hyperIdx])
+        ax.set_title(hyperVars[hyperIdx]["Name"])
+    ax = fig.add_subplot(max(numLatent, numHyper) + 1, 2, 2 * max(numLatent, numHyper) + 1  )
+    ax.plot(generations, gammas)
+    ax.set_title("gamma")
 
     return fig, ax
+
 
 
 
