9f4e852b00781aa38898b1cac80b9d98adf00ddc
Sergio Martin <martiser@ethz.ch>
2020-04-30 20:37:38 +0200

    Added dynamic programming

--- tutorials/basic/10.reinforcement.learning/run-alphabeta.py
+++ tutorials/basic/10.reinforcement.learning/run-alphabeta.py
@@ -23,35 +23,19 @@ def h(v):
 # Environment that provides the state/action loop
 def environment(k):
 
-  # Initialize X as per problem's specifications
-  x = initialX 
-
-  # Initializing reward
-  k["Reward"] = 0.0    
+ # Getting Budget (x) and amount to spend (y).
+ x = k["State"][0]
+ y = k["Action"][0]
   
-  # Run the policy for N stages
-  for i in range(N):
-
-   # Setting current State
-   k["State"] = [ x ]
-   
-   # Get back to Korali to obtain the next action to perform
-   k.update()
-  
-   # Getting Action (the value of Y)
-   y = k["Action"][0] 
-  
-   # If Y greater than current X, then this is not a feasible policy, returning -infinite reward
-   if (y > x):
-    k["Reward"] = -math.inf
-    return
+ # If Y greater than current X, then this is not a feasible policy, returning -infinite reward
+ if (y > x): k["Reward"] = -math.inf
     
-   # Else, we use the given function to calculate reward
-   k["Reward"] = k["Reward"] + g(y) + h(x-y)
+ # Else, we use the given function to calculate reward
+ else: k["Reward"] = g(y) + h(x-y)
   
-   # Calculating new value for X
-   x = alpha*y + beta*(x-y)
-  
+ # Calculating new state (remaining budget)
+ k["State"] = [ alpha*y + beta*(x-y) ]
+   
 ######## Configuring Korali Experiment
 
 import korali
@@ -80,18 +64,14 @@ e["Variables"][1]["Upper Bound"] = initialX
 e["Variables"][1]["Granularity"] = granularity
 
 # Configuring the solver
-e["Solver"]["Type"] = "Learner/QTable"
-e["Solver"]["Q Update Algorithm"] = "Q-Learning"
-e["Solver"]["Initial Q Value"] = 50
-
-# Configuring Output
-e["Console Output"]["Frequency"] = 5000
-e["File Output"]["Enabled"] = False
+e["Solver"]["Type"] = "Dynamic Programming"
+e["Solver"]["Recursion Depth"] = N
+e["Solver"]["Termination Criteria"]["Max Generations"] = 1
 
 ######## Running Korali and printing results
 
 k = korali.Engine()
 k.run(e)
 
-print('Best Policy:     ' + str(e["Results"]["Optimal Policy Actions"]))
-print('Optimal Reward:  ' + str(e["Results"]["Optimal Reward"]))
+#print('Best Policy:     ' + str(e["Results"]["Optimal Policy Actions"]))
+#print('Optimal Reward:  ' + str(e["Results"]["Optimal Reward"]))
