b0b12b6d3f1e06eeeecee7c5540ab4dcc6acab50
Sergio Martin <martiser@ethz.ch>
2020-05-07 12:56:18 +0200

    Adding personalized test scripts

---
+++ tutorials/basic/1.optimization/.run_test.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+###### Auxiliar Functions and Variables #########
+
+source ../../../tests/functions.sh
+
+##### Getting Test Type
+
+getTestMode $1
+
+##### Deleting Previous Results
+
+logEcho "  + Deleting previous results..."
+rm -rf _korali_result*
+check_result
+
+##### Running Tests
+
+for file in run-*.py
+do
+
+ if [ ${file: -3} == ".py" ]; then
+ 
+   logEcho "  + Processing File: $file"
+ 
+   testFile=__test${file}
+   logEcho "  + Creating Test File: $testFile"
+ 
+   cp $file $testFile
+   check_result
+   
+   if [[ $testMode == "fast" ]]; then
+     logEcho "  + Modifying test file for fast execution: $testFile"
+     sed  -e 's%\["Max Generations"\] =%["Max Generations"] = 5 #%g' $file > $testFile 
+     check_result
+   fi
+ 
+   logEcho "  + Running Test File: $testFile"
+   python3 $testFile >> $logFile 2>&1
+   check_result
+ 
+   logEcho "  + Deleting Test File"
+   rm $testFile
+   check_result
+ fi
+  
+done
--- tutorials/basic/10.reinforcement.learning/.run_test.sh
+++ tutorials/basic/10.reinforcement.learning/.run_test.sh
@@ -6,48 +6,42 @@ source ../../../tests/functions.sh
 
 ##### Getting Test Type
 
-testMode=undefined
+getTestMode $1
 
-if [[ "$1" == "--fast" ]]; then testMode="fast"; fi
-if [[ "$1" == "--full" ]]; then testMode="full"; fi
+##### Deleting Previous Results
 
-if [[ $testMode == "undefined" ]]; then
- echo "Error: must define whether the test is meant to run in --fast or --full mode."
- exit -1
-fi
-
+logEcho "  + Deleting previous results..."
+rm -rf _korali_result*
+check_result
 
 ##### Running Tests
 
 for file in run-*.py
-  do
+do
+  if [ ${file: -3} == ".py" ]; then
 
-    if [ ${file: -3} == ".py" ]; then
+    logEcho "  + Processing File: $file"
 
-      logEcho "  + Processing File: $file"
+    testFile=__test${file}
+    logEcho "  + Creating Test File: $testFile"
 
-      testFile=__test${file}
-      logEcho "  + Creating Test File: $testFile"
-
-      if [[ $testMode == "fast" ]]; then
-         sed  -e 's%granularity = 0.0%granularity = 0.2 #%g' \
-              -e 's%granularity = 1%granularity = 20 #%g' \
-              $file > $testFile 
-         check_result
-      fi
-
-      if [[ $testMode == "full" ]]; then
-         cp $file $testFile
-         check_result
-      fi
-
-      logEcho "  + Running Test File: $testFile"
-      python3 $testFile >> $logFile 2>&1
-      check_result
-
-      logEcho "  + Deleting Test File"
-      rm $testFile
+    cp $file $testFile
+    check_result
+       
+    if [[ $testMode == "fast" ]]; then
+      logEcho "  + Modifying test file for fast execution: $testFile"
+      sed  -e 's%granularity = 0.0%granularity = 0.2 #%g' \
+           -e 's%granularity = 1%granularity = 20 #%g' \
+           $file > $testFile 
       check_result
     fi
-    
-  done
+
+    logEcho "  + Running Test File: $testFile"
+    python3 $testFile >> $logFile 2>&1
+    check_result
+
+    logEcho "  + Deleting Test File"
+    rm $testFile
+    check_result
+  fi
+done
