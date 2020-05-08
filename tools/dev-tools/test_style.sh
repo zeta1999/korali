58d062a6530af48d22ef03db866050f32393c107
Sergio Martin <martiser@ethz.ch>
2020-05-08 11:23:54 +0200

    Updating style test

--- tools/dev-tools/test_style.sh
+++ tools/dev-tools/test_style.sh
@@ -7,6 +7,13 @@ fi
 
 runClangCmd=run-clang-format/run-clang-format.py
 
-command="python3 $runClangCmd --clang-format-executable $clangFormatCmd -r ../../source --extensions _cpp,_hpp"
+python3 $runClangCmd --clang-format-executable $clangFormatCmd -r ../../source --extensions _cpp,_hpp > /dev/null
 
-$command
+if [ ! $? -eq 0 ]; then
+ echo "[Korali] C++ Code formatting is incorrect. Please run tools/dev-tools/correct_style.sh to fix it."
+ exit -1
+else
+ echo "[Korali] C++ Code formatting is correct."
+ exit 0
+fi
+
