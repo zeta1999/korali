fc497b92c4f667958e5319c7eb9dc4d8bb61174b
Sergio Martin <martiser@ethz.ch>
2020-05-14 15:29:35 +0200

    Updating NN

--- tutorials/basic/13.supervisedLearning/run-nn.py
+++ tutorials/basic/13.supervisedLearning/run-nn.py
@@ -3,34 +3,25 @@ import os
 import sys
 import korali
 
-# Defining Training Set [[x1,y1,q1],[x2,y2,q2]]
-tSet = [ [ 0.2, 0.3, 15.3 ], [ 0.7, 0.2, 35.3 ]  ]
+# Defining Training Sets
+tInputSet  = [ [ 0.2, 0.3 ], [ 0.7, 0.2 ]  ]
+tOutputSet = [ [ 15.3 ], [ 35.3 ]  ]
 
 # Describing Korali Experiment
 e = korali.Experiment()
 
 e["Problem"]["Type"] = "Supervised Learning"
-e["Problem"]["Training Set"] = tSet 
-e["Problem"]["Loss Function"] = "Mean Squared Error"
-
-# Configuring Input/Output Variables
-e["Variables"][0]["Name"] = "X"
-e["Variables"][0]["Type"] = "Input"
-
-e["Variables"][1]["Name"] = "Y"
-e["Variables"][1]["Type"] = "Input"
-
-e["Variables"][2]["Name"] = "Q"
-e["Variables"][2]["Type"] = "Output"
-
-# Configuring Solver
-
 e["Solver"]["Type"] = "Neural Network"
+
 e["Solver"]["Operation"] = "Train"
 e["Solver"]["Engine Kind"] = "CPU"
 e["Solver"]["Optimizer"] = "CMAES"
 e["Solver"]["Termination Criteria"]["Max Generations"] = 1
-e["Solver"]["Weight Initialization"] = "Xavier" 
+e["Solver"]["Weight Initialization"] = "Xavier"
+e["Solver"]["Loss Function"] = "Mean Squared Error" 
+
+e["Solver"]["Training Set Input"]  = tInputSet
+e["Solver"]["Training Set Output"] = tOutputSet
 
 e["Solver"]["Layers"][0]["Type"] = "Input"
 e["Solver"]["Layers"][0]["Node Count"] = 2
@@ -51,12 +42,3 @@ e["Solver"]["Layers"][3]["Activation Function"] = "Identity"
 k = korali.Engine()
 k.run(e)
 
-# Defining Forward Set [[x1,y1],[x2,y2]]
-fSet = [ [ 0.4, 0.9 ], [ 0.1, 0.0 ]  ]
-
-e["Problem"]["Forward Set"] = fSet 
-e["Solver"]["Operation"] = "Forward"
-
-k.run(e)
-
-#outputs = e["Results"]["Outputs"]
