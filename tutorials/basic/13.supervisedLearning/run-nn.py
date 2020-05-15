717f50602699024a649b08b7fe3378d7fce5867e
Sergio Martin <martiser@ethz.ch>
2020-05-15 10:40:22 +0200

    finished forwarding example for XOR

--- tutorials/basic/13.supervisedLearning/run-nn.py
+++ tutorials/basic/13.supervisedLearning/run-nn.py
@@ -4,8 +4,7 @@ import sys
 import korali
 
 # Defining Training Sets
-tInputSet  = [ [ 0.2, 0.3 ], [ 0.7, 0.2 ]  ]
-tOutputSet = [ [ 15.3 ], [ 35.3 ]  ]
+tInputSet  = [ [ 0.0, 0.0 ], [ 0.0, 1.0 ], [ 1.0, 0.0 ], [ 1.0, 1.0 ]  ]
 
 # Describing Korali Experiment
 e = korali.Experiment()
@@ -13,31 +12,24 @@ e = korali.Experiment()
 e["Problem"]["Type"] = "Supervised Learning"
 e["Solver"]["Type"] = "Neural Network"
 
-e["Solver"]["Operation"] = "Train"
-e["Solver"]["Engine Kind"] = "CPU"
-e["Solver"]["Optimizer"] = "CMAES"
-e["Solver"]["Termination Criteria"]["Max Generations"] = 1
-e["Solver"]["Weight Initialization"] = "Xavier"
-e["Solver"]["Loss Function"] = "Mean Squared Error" 
-
-e["Solver"]["Training Set Input"]  = tInputSet
-e["Solver"]["Training Set Output"] = tOutputSet
+e["Solver"]["Operation"] = "Forward"
+e["Solver"]["Batch Input"]  = tInputSet 
 
 e["Solver"]["Layers"][0]["Type"] = "Input"
 e["Solver"]["Layers"][0]["Node Count"] = 2
 e["Solver"]["Layers"][0]["Activation Function"] = "Identity"
 
 e["Solver"]["Layers"][1]["Type"] = "Dense"
-e["Solver"]["Layers"][1]["Node Count"] = 80
-e["Solver"]["Layers"][1]["Activation Function"] = "SoftSign"
+e["Solver"]["Layers"][1]["Node Count"] = 2
+e["Solver"]["Layers"][1]["Activation Function"] = "ReLU"
+e["Solver"]["Layers"][1]["Weights"] = [ [ 1.0, 1.0 ], [ 1.0, 1.0 ] ]
+e["Solver"]["Layers"][1]["Bias"] = [ 0.0, -1.0 ] 
 
-e["Solver"]["Layers"][2]["Type"] = "Dense"
-e["Solver"]["Layers"][2]["Node Count"] = 60
-e["Solver"]["Layers"][2]["Activation Function"] = "SoftMax"
-
-e["Solver"]["Layers"][3]["Type"] = "Output"
-e["Solver"]["Layers"][3]["Node Count"] = 1
-e["Solver"]["Layers"][3]["Activation Function"] = "Identity"
+e["Solver"]["Layers"][2]["Type"] = "Output"
+e["Solver"]["Layers"][2]["Node Count"] = 1
+e["Solver"]["Layers"][2]["Activation Function"] = "Identity"
+e["Solver"]["Layers"][2]["Weights"] = [ [ 1.0, -2.0 ] ]
+e["Solver"]["Layers"][2]["Bias"] = [ 0.0 ]
 
 k = korali.Engine()
 k.run(e)
