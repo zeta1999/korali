58488b70451e70d9aa66cae788d62414847b18bb
Sergio Martin <martiser@ethz.ch>
2020-05-19 18:35:17 +0200

    Adding deep supervisor solver and updating supervised learning problem

--- tutorials/basic/13.supervisedLearning/run-sin.py
+++ tutorials/basic/13.supervisedLearning/run-sin.py
@@ -7,81 +7,48 @@ import numpy as np
 # Defining Training Sets
 
 tInputSet  = np.random.uniform(0,2*np.pi,100)
+vInputSet  = np.random.uniform(0,2*np.pi,20)
+
 tOutputSet = np.sin(tInputSet)
+vOutputSet = np.sin(vInputSet)
 
-tInputSet = [ [ i ] for i in tInputSet.tolist() ]
-tOutputSet = [ [ i ] for i in tOutputSet.tolist() ]  
+trainingSet = list(zip(tInputSet.tolist(), tOutputSet.tolist()))
+validationSet = list(zip(tInputSet.tolist(), tOutputSet.tolist()))
 
-# Describing Neural Network-based Model
-def neuralModel(sample, input, output):
- e = korali.Experiment()
- 
- e["Problem"]["Type"] = "Supervised Learning"
- e["Solver"]["Type"] = "Neural Network"
- 
- p = sample["Parameters"]
- 
- e["Solver"]["Layers"][0]["Type"] = "Input"
- e["Solver"]["Layers"][0]["Node Count"] = 1
- e["Solver"]["Layers"][0]["Node Values"] = input
- e["Solver"]["Layers"][0]["Activation Function"] = "Identity"
- 
- e["Solver"]["Layers"][1]["Type"] = "Dense"
- e["Solver"]["Layers"][1]["Node Count"] = 4
- e["Solver"]["Layers"][1]["Weights"] = [ [ p[0] ], [ p[1] ], [ p[2] ], [ p[3] ] ] 
- e["Solver"]["Layers"][1]["Bias"] = [ p[4], p[5], p[6], p[7] ]
- e["Solver"]["Layers"][1]["Activation Function"] = "Tanh"
- 
- e["Solver"]["Layers"][2]["Type"] = "Dense"
- e["Solver"]["Layers"][2]["Node Count"] = 4
- e["Solver"]["Layers"][2]["Weights"] = [ [ p[8], p[9], p[10], p[11] ], [ p[12], p[13], p[14], p[15] ], [ p[16], p[17], p[18], p[19] ], [ p[20], p[21], p[22], p[23] ]  ]
- e["Solver"]["Layers"][2]["Bias"] = [ p[24], p[25], p[26], p[27] ]
- e["Solver"]["Layers"][2]["Activation Function"] = "Tanh"
- 
- e["Solver"]["Layers"][3]["Type"] = "Output"
- e["Solver"]["Layers"][3]["Node Count"] = 1
- e["Solver"]["Layers"][3]["Weights"] = [ [ p[28],  p[29], p[30],  p[31] ] ]
- e["Solver"]["Layers"][3]["Bias"] = [ p[32] ]
- e["Solver"]["Layers"][3]["Activation Function"] = "Identity"
- 
- e["Solver"]["Termination Criteria"]["Max Generations"] = 1
- e["File Output"]["Enabled"] = False
- e["Console Output"]["Verbosity"] = "Silent"
- 
- k = korali.Engine()
- k.run(e)
-
- ## Calculating MSE
- 
- nnOut = e["Solver"]["Layers"][3]["Node Values"] 
- 
- meanSquareError = 0
- for i in range(len(nnOut)):
-  for j in range(len(nnOut[i])):
-   error = output[i][j] - nnOut[i][j]
-   meanSquareError += error*error
- meanSquareError = meanSquareError / (len(nnOut) * len(nnOut[0]))
- 
- sample["F(x)"] = -meanSquareError
- 
-# Creating new experiment
 e = korali.Experiment()
 
-# Configuring Problem
-e["Problem"]["Type"] = "Optimization/Stochastic"
-e["Problem"]["Objective Function"] = lambda x : neuralModel(x, tInputSet, tOutputSet) 
+e["Problem"]["Type"] = "Supervised Learning"
+e["Problem"]["Training Set"] = trainingSet
+e["Problem"]["Validation Set"] = validationSet
+ 
+e["Variables"][0]["Name"] = "X"
+e["Variables"][0]["Type"] = "Input"
 
-# Defining the problem's variables.
-for i in range(33):
- e["Variables"][i]["Name"] = "X" + str(i)
- e["Variables"][i]["Lower Bound"] = -10.0
- e["Variables"][i]["Upper Bound"] = +10.0
+e["Variables"][1]["Name"] = "Sin(X)"
+e["Variables"][1]["Type"] = "Output"
 
-# Configuring CMA-ES parameters
-e["Solver"]["Type"] = "CMAES"
-e["Solver"]["Population Size"] = 50 
-e["Solver"]["Termination Criteria"]["Min Value Difference Threshold"] = 1e-9
+e["Solver"]["Type"] = "Deep Supervisor"
+e["Solver"]["Optimizer"] = "CMAES"
 
-# Running Korali
+e["Solver"]["Neural Network"]["Layers"][0]["Type"] = "Input"
+e["Solver"]["Neural Network"]["Layers"][0]["Node Count"] = 1
+e["Solver"]["Neural Network"]["Layers"][0]["Activation Function"] = "Identity"
+
+e["Solver"]["Neural Network"]["Layers"][1]["Type"] = "Dense"
+e["Solver"]["Neural Network"]["Layers"][1]["Node Count"] = 4
+e["Solver"]["Neural Network"]["Layers"][1]["Activation Function"] = "Tanh"
+
+e["Solver"]["Neural Network"]["Layers"][2]["Type"] = "Dense"
+e["Solver"]["Neural Network"]["Layers"][2]["Node Count"] = 4
+e["Solver"]["Neural Network"]["Layers"][2]["Activation Function"] = "Tanh"
+
+e["Solver"]["Neural Network"]["Layers"][3]["Type"] = "Output"
+e["Solver"]["Neural Network"]["Layers"][3]["Node Count"] = 1
+e["Solver"]["Neural Network"]["Layers"][3]["Activation Function"] = "Identity"
+
+e["File Output"]["Enabled"] = False
+e["Console Output"]["Verbosity"] = "Silent"
+
 k = korali.Engine()
-#k.run(e)
+k.run(e)
+
