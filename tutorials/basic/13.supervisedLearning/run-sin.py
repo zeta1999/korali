f4c402c122bff0419812225b97f1e1ccf80bd6fe
Sergio Martin <martiser@ethz.ch>
2020-05-25 12:35:31 +0200

    Fixing deep learning module

--- tutorials/basic/13.supervisedLearning/run-sin.py
+++ tutorials/basic/13.supervisedLearning/run-sin.py
@@ -9,8 +9,8 @@ k = korali.Engine()
 
 # Defining Training Sets
 np.random.seed(0xC0FFEE)
-trainingInputSet  = np.random.uniform(0,2*np.pi,500)
-validationInputSet  = np.random.uniform(0,2*np.pi,500)
+trainingInputSet  = np.random.uniform(0,2*np.pi,200)
+validationInputSet  = np.random.uniform(0,2*np.pi,200)
 
 trainingOutputSet = np.sin(trainingInputSet)
 validationOutputSet = np.sin(validationInputSet)
@@ -34,9 +34,8 @@ e["Variables"][1]["Validation Data"] = validationOutputSet.tolist()
 ### Using a neural network solver (deep learning) for inference
 
 e["Solver"]["Type"] = "Deep Learning"
-
+e["Solver"]["Termination Criteria"]["Max Inactive Steps"] = 50
 e["Solver"]["Neural Network"]["Optimizer"]["Type"] = "CMAES"
-e["Solver"]["Neural Network"]["Optimizer"]["Population Size"] = 32
 
 ### Defining the shape of the neural network
 
@@ -45,16 +44,12 @@ e["Solver"]["Neural Network"]["Layers"][0]["Node Count"] = 1
 e["Solver"]["Neural Network"]["Layers"][0]["Activation Function"] = "Identity"
 
 e["Solver"]["Neural Network"]["Layers"][1]["Type"] = "Dense"
-e["Solver"]["Neural Network"]["Layers"][1]["Node Count"] = 5
+e["Solver"]["Neural Network"]["Layers"][1]["Node Count"] = 10
 e["Solver"]["Neural Network"]["Layers"][1]["Activation Function"] = "Tanh"
 
-e["Solver"]["Neural Network"]["Layers"][2]["Type"] = "Dense"
-e["Solver"]["Neural Network"]["Layers"][2]["Node Count"] = 5
-e["Solver"]["Neural Network"]["Layers"][2]["Activation Function"] = "Tanh"
-
-e["Solver"]["Neural Network"]["Layers"][3]["Type"] = "Output"
-e["Solver"]["Neural Network"]["Layers"][3]["Node Count"] = 1
-e["Solver"]["Neural Network"]["Layers"][3]["Activation Function"] = "Identity"
+e["Solver"]["Neural Network"]["Layers"][2]["Type"] = "Output"
+e["Solver"]["Neural Network"]["Layers"][2]["Node Count"] = 1
+e["Solver"]["Neural Network"]["Layers"][2]["Activation Function"] = "Identity"
 
 e["Random Seed"] = 0xC0FFEE
 
@@ -64,7 +59,7 @@ k.run(e)
 
 ### Obtaining inferred results from the NN and comparing them to the actual solution
 
-testInputSet = np.random.uniform(0,2*np.pi,500)
+testInputSet = np.random.uniform(0,2*np.pi,200)
 testInputSet = [ [ x ] for x in testInputSet.tolist() ]
 
 testInferredSet = e.test(testInputSet)
