87b3d92539a485b4c8556f8fb763a50b600b7e94
Sergio Martin <martiser@ethz.ch>
2020-05-29 15:38:38 +0200

    Added dev tools to the prereqs scripts

--- tools/dev-tools/correct_style.sh
+++ tools/dev-tools/correct_style.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 # File       : correct_style
 # Created    : Mon Jan 20 2020 11:29:59 AM (+0100)
-# Author     : Fabian Wermelinger (adjusted to Korali by Sergio Martin)
+# Author     : Fabian Wermelinger (adjusted to Korali by Sergio Martin and Lucas Amoudruz)
 # Description: Utility to adjust source code according to style conventions
 # Copyright 2020 ETH Zurich. All Rights Reserved.
 
-clangFormat=llvm/bin/clang-format
-if [ ! -f $clangFormat ]; then
- clangFormat=clang-format
+# If clang-format is not installed, run the installation script
+clangFormatBin=$PWD/../../prereqs/clang-format
+if [ ! -f $clangFormatBin ]; then
+ pushd ../..
+ ./prereqs/install_clang.sh
+ popd
 fi
 
 root="../../source"
@@ -15,4 +18,4 @@ root="../../source"
 src_files=`find $root -type f -name "*pp" -not -path "$root/external/*"`
 
 echo $src_files | \
-    xargs -n6 -P2 $clangFormat -style=file -i "$@"
+    xargs -n6 -P2 $clangFormatBin -style=file -i "$@"
--- tools/dev-tools/test_style.sh
+++ tools/dev-tools/test_style.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
-clangFormatCmd=llvm/bin/clang-format
+# If clang-format is not installed, run the installation script
+clangFormatBin=$PWD/../../prereqs/clang-format
+if [ ! -f $clangFormatBin ]; then
+ pushd ../..
+ ./prereqs/install_clang.sh
+ popd
+fi
+
+# If run-clang-format is not installed, clone it
+if [ ! -f  run-clang-format/run-clang-format.py ]; then
+
+ git clone https://github.com/Sarcasm/run-clang-format.git
+ if [ ! $? -eq 0 ]; then
+  echo "[Korali] Error installing run-clang-format."
+  exit 1
+ fi
+
+fi
+
+clangFormatCmd=../../prereqs/clang-format
 if [ ! -f $clangFormatCmd ]; then
  clangFormatCmd=clang-format
 fi
