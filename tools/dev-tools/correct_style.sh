4b1c222f0d4b94a7522856f0a6c3b5a5f491105e
Sergio Martin <martiser@ethz.ch>
2020-05-31 11:39:32 +0200

    Adding yapf for python formatting

--- tools/dev-tools/correct_style.sh
+++ tools/dev-tools/correct_style.sh
@@ -5,22 +5,51 @@
 # Description: Utility to adjust source code according to style conventions
 # Copyright 2020 ETH Zurich. All Rights Reserved.
 
+function check()
+{
+ if [ ! $? -eq 0 ]
+ then
+  echo "[Korali] Error fixing style."
+  exit -1
+ fi 
+}
+
 fileDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $fileDir
+
+root="../../"
+
+##############################################
+### Correcting C++ Code Style
+##############################################
 
 # If clang-format is not installed, run the installation script
 clangFormatBin=$PWD/../../prereqs/clang-format
 if [ ! -f $clangFormatBin ]; then
  pushd ../..
  ./prereqs/install_clang.sh
+ check
  popd
 fi
-
-root="../../"
 
 src_files=`find $root -type f -name "*pp" -not -path "$root/external/*"`
 
 echo $src_files | \
     xargs -n6 -P2 $clangFormatBin -style=file -i "$@"
+
+check
+
+##############################################
+### Correcting Python Code Style
+##############################################
+
+PIP_USER=$(python3 -c "import sys; hasattr(sys, 'real_prefix') or print('--user')")
+
+python3 -m yapf --version
+if [ $? -ne 0 ]; then
+
+  echo "[Korali] yapf not found, trying to install it automatically."
+  python3 -m pip install $PIP_USER yapf >> $logFile 2>&1; check
+fi
 
 popd
