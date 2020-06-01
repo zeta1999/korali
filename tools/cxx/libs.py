f5af26564a9a409e5d637eefc1f8130476e3877d
Sergio Martin <martiser@ethz.ch>
2020-06-01 18:01:51 +0200

    Fixing style

--- tools/cxx/libs.py
+++ tools/cxx/libs.py
@@ -3,9 +3,11 @@ def getlibs(extdir, makeFlags):
   pythonLibPaths = [
       p for p in makeFlags['PYBIND11LIBS'].split() if p.startswith('-L')
   ]
-  for p in pythonLibPaths:  flags += ' -Wl,-rpath,' + p.replace('-L', '') + ' '
+  for p in pythonLibPaths:
+    flags += ' -Wl,-rpath,' + p.replace('-L', '') + ' '
   flags += '-L' + extdir + ' -lkorali -Wl,-rpath,' + extdir + ' '
-  flags += '-L' + makeFlags['GSLPREFIX'] + '/lib -Wl,-rpath,' + makeFlags['GSLPREFIX'] + '/lib '
+  flags += '-L' + makeFlags['GSLPREFIX'] + '/lib -Wl,-rpath,' + makeFlags[
+      'GSLPREFIX'] + '/lib '
   flags += '-lpython3'
   flags += ' ' + makeFlags['ONEDNNLIBS']
   flags += ' ' + makeFlags['LIBGPLIBS']
