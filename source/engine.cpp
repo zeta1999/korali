67948e7684a6172b77d35d616fe946c59ecb7c2d
Sergio Martin <martiser@ethz.ch>
2020-06-01 09:12:07 +0200

    Adding a flag that checks for python, to prevent break checking if python is not active

--- source/engine.cpp
+++ source/engine.cpp
@@ -11,6 +11,7 @@
 
 std::vector<std::function<void(korali::Sample &)> *> korali::_functionVector;
 std::stack<korali::Engine *> korali::_engineStack;
+bool korali::isPythonActive = 0;
 
 korali::Engine::Engine()
 {
@@ -276,10 +277,22 @@ PYBIND11_MODULE(libkorali, m)
 
   pybind11::class_<korali::Engine>(m, "Engine")
     .def(pybind11::init<>())
-    .def("run", pybind11::overload_cast<korali::Experiment &>(&korali::Engine::run))
-    .def("run", pybind11::overload_cast<std::vector<korali::Experiment> &>(&korali::Engine::run))
-    .def("resume", pybind11::overload_cast<korali::Experiment &>(&korali::Engine::resume))
-    .def("resume", pybind11::overload_cast<std::vector<korali::Experiment> &>(&korali::Engine::resume))
+    .def("run", [](korali::Engine &k, korali::Experiment &e) {
+      korali::isPythonActive = true;
+      k.run(e);
+    })
+    .def("run", [](korali::Engine &k, std::vector<korali::Experiment> &e) {
+      korali::isPythonActive = true;
+      k.run(e);
+    })
+    .def("resume", [](korali::Engine &k, korali::Experiment &e) {
+      korali::isPythonActive = true;
+      k.resume(e);
+    })
+    .def("resume", [](korali::Engine &k, std::vector<korali::Experiment> &e) {
+      korali::isPythonActive = true;
+      k.resume(e);
+    })
     .def("__getitem__", pybind11::overload_cast<pybind11::object>(&korali::Engine::getItem), pybind11::return_value_policy::reference)
     .def("__setitem__", pybind11::overload_cast<pybind11::object, pybind11::object>(&korali::Engine::setItem), pybind11::return_value_policy::reference);
 
