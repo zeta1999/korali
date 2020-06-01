0d8e7c577c5b263c4fd6ec40532827c6c5e51e54
Sergio Martin <martiser@ethz.ch>
2020-06-01 17:54:08 +0200

    Adjusting C++ compilation and macos library paths

--- tools/cxx/cflags.py
+++ tools/cxx/cflags.py
@@ -1,5 +1,12 @@
 def getcflags(extdir, makeFlags):
   fixedflags = '-g -O3 -Wno-deprecated-declarations -I' + extdir
-  print(fixedflags + ' ' + makeFlags['OPTFLAGS'] + ' ' + makeFlags['MPIFLAGS'] +
-        ' ' + makeFlags['GSLCFLAGS'] + ' ' + makeFlags['CXXARCH'] + ' ' +
-        makeFlags['PYBIND11INCLUDES'])
+  print(fixedflags + ' '
+        + makeFlags['OPTFLAGS']
+        + ' ' + makeFlags['MPIFLAGS']
+        + ' ' + makeFlags['GSLCFLAGS']
+        + ' ' + makeFlags['RTNORMCFLAGS']
+        + ' ' + makeFlags['LIBCOCFLAGS']
+        + ' ' + makeFlags['EIGENCFLAGS']
+        + ' ' + makeFlags['ONEDNNCFLAGS']
+        + ' ' + makeFlags['CXXARCH']
+        + ' ' + makeFlags['PYBIND11INCLUDES'])
--- tools/cxx/libs.py
+++ tools/cxx/libs.py
@@ -3,10 +3,12 @@ def getlibs(extdir, makeFlags):
   pythonLibPaths = [
       p for p in makeFlags['PYBIND11LIBS'].split() if p.startswith('-L')
   ]
-  for p in pythonLibPaths:
-    flags += ' -Wl,-rpath,' + p.replace('-L', '') + ' '
+  for p in pythonLibPaths:  flags += ' -Wl,-rpath,' + p.replace('-L', '') + ' '
   flags += '-L' + extdir + ' -lkorali -Wl,-rpath,' + extdir + ' '
-  flags += '-L' + makeFlags['GSLPREFIX'] + '/lib -Wl,-rpath,' + makeFlags[
-      'GSLPREFIX'] + '/lib '
+  flags += '-L' + makeFlags['GSLPREFIX'] + '/lib -Wl,-rpath,' + makeFlags['GSLPREFIX'] + '/lib '
   flags += '-lpython3'
+  flags += ' ' + makeFlags['ONEDNNLIBS']
+  flags += ' ' + makeFlags['LIBGPLIBS']
+  flags += ' ' + makeFlags['RTNORMLIBS']
+  flags += ' ' + makeFlags['LIBCOLIBS']
   print(flags + ' ' + makeFlags['GSLLIBS'] + ' ' + makeFlags['PYBIND11LIBS'])
