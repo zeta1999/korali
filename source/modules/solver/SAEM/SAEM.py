5708202cc8b6a33e41686cd324b339410bac3c11
Susi <kellersu@student.ethz.ch>
2020-06-02 13:21:20 +0200

    fix merge conflict in SAEM plotting and remove wrong version completely

--- source/modules/solver/SAEM/SAEM.py
+++ source/modules/solver/SAEM/SAEM.py
@@ -43,7 +43,6 @@ def plotVariables(genList):
     hyperparams[idx, ...] = genList[i]["Solver"]["Current Hyperparameters"]
     gammas[idx] = genList[i]["Solver"]["Gamma"]
 
-# <<<<<<< HEAD
   # Create an "outer" subplot for the two column titles
   fig, ax0 = plt.subplots(nrows=1, ncols=2, figsize=(8, 20), dpi=60)
   ax0[0].set_title("Latent Variables", y=1.03, fontdict={'fontweight': 8, 'fontsize': 20})
@@ -84,28 +83,6 @@ def plotVariables(genList):
   ax = fig.add_subplot(max(numLatent, numHyper) + 1, 2, 2 * max(numLatent, numHyper) + 1)
   ax.plot(generations, gammas)
   ax.set_title("gamma")
-# =======
-#   fig, ax = plt.subplots(
-#       nrows=max(numLatent, numHyper) + 1, ncols=2, figsize=(8, 20), dpi=60)
-#   fig.subplots_adjust(hspace=.8)
-#   # samplesTmp = np.reshape( samples, (numentries,numdim) )
-#   plt.suptitle(
-#       'SAEM Plotter - \nNumber of Samples {0}\n'.format(str(numSamples)),
-#       fontweight='bold',
-#       fontsize=12)
-#   for latentIdx in range(numLatent):
-#     ax[latentIdx, 0].errorbar(
-#         x=generations,
-#         y=sample_means[:, latentIdx],
-#         yerr=sample_sdevs[:, latentIdx])
-#     ax[latentIdx, 0].set_title(latentVars[latentIdx]["Name"])
-#   # fig, ax = plt.subplots(nrows=max(numLatent, numHyper) + 1, ncols=1, figsize=(15,8))
-#   for hyperIdx in range(numHyper):
-#     ax[hyperIdx, 1].plot(generations, hyperparams[:, hyperIdx])
-#     ax[hyperIdx, 1].set_title(hyperVars[hyperIdx]["Name"])
-#   ax[-1, 0].plot(generations, gammas)
-#   ax[-1, 0].set_title("gamma")
-# >>>>>>> master
 
   return fig, ax
 
