c82f529d5b1f0b583e967c85ad1bd08db9e12835
Susi <kellersu@student.ethz.ch>
2020-06-04 11:50:33 +0200

    continued debugging; tutorial runs now as well (probably still computational errors to be found)

--- tutorials/advanced/10.hierarchical.latent.cxx/run-saem-hierarchical.cpp
+++ tutorials/advanced/10.hierarchical.latent.cxx/run-saem-hierarchical.cpp
@@ -40,7 +40,7 @@ int main(int argc, char* argv[])
   e["Problem"][ "Latent Space Dimensions" ] = 1;
 
   e["Solver"]["Type"] = "HSAEM";
-  e["Solver"]["Number Samples Per Step"] = 100;
+  e["Solver"]["Number Samples Per Step"] = 10; // reduce further to speed up
   e["Solver"]["Termination Criteria"]["Max Generations"] = 30;
 
   e["Distributions"][0]["Name"] = "Uniform 0";
