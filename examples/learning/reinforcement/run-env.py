59897e478a335093cc83b56f0cdd5cb95e62fca9
Sergio Martin <martiser@ethz.ch>
2020-06-29 09:31:22 +0200

    Optimizing Neural Nets and DQN

--- examples/learning/reinforcement/run-env.py
+++ examples/learning/reinforcement/run-env.py
@@ -32,9 +32,18 @@ def env(s):
   print(' + State:  ' + str(x))
   print(' + Action: ' + str(y))
   
+  # If y less than zero, no reward 
+  if (y < 0): 
+   s["Reward"] = 0
+   return
+   
+  # If y if higher than x, then return zero 
+  if (y > x): 
+   s["Reward"] = 0
+   return
+   
   # Calcuating reward
-  p = 0.5*x
-  reward = -(5/x)*(p-y)*(p-y) + 1
+  reward = (y*y)/3
      
   s["Reward"] = reward
   print(' + Reward: ' + str(reward))
@@ -58,7 +67,7 @@ e = korali.Experiment()
 
 e["Problem"]["Type"] = "Reinforcement Learning"
 e["Problem"]["Environment Function"] = env
-e["Problem"]["Initial States"] = [ [ initialX ] for i in range(50000) ]
+e["Problem"]["Initial States"] = [ [ initialX ] for i in range(1000000) ]
 
 e["Variables"][0]["Name"] = "X"
 e["Variables"][0]["Type"] = "State"
@@ -71,9 +80,10 @@ e["Variables"][1]["Upper Bound"] = 1.0
 ### Using a neural network solver (deep learning) for inference
 
 e["Solver"]["Type"] = "DQN"
-e["Solver"]["Replay Start Size"] = 5000
-e["Solver"]["Replay Memory Size"] = 15000
-e["Solver"]["Mini Batch Size"] = 32
+e["Solver"]["Replay Start Size"] = 50
+e["Solver"]["Replay Memory Size"] = 1000
+e["Solver"]["Agent History Size"] = 2
+e["Solver"]["Mini Batch Size"] = 4
 e["Solver"]["Optimization Steps Per Update"] = 10
 e["Solver"]["Discount Factor"] = 0.99
 
@@ -81,7 +91,11 @@ e["Solver"]["Discount Factor"] = 0.99
 
 e["Solver"]["Action Optimizer"]["Type"] = "Optimizer/Adam"
 e["Solver"]["Action Optimizer"]["Termination Criteria"]["Max Gradient Norm"] = 1000000000
+#e["Solver"]["Action Optimizer"]["Termination Criteria"]["Min Gradient Norm"] = 0.0000000010
+
 e["Solver"]["Weight Optimizer"]["Type"] = "Optimizer/Adam"
+e["Solver"]["Weight Optimizer"]["Termination Criteria"]["Max Gradient Norm"] = 1000000000
+e["Solver"]["Weight Optimizer"]["Termination Criteria"]["Min Gradient Norm"] = 0.0000000000001
 
 e["Solver"]["Neural Network"]["Batch Normalization"]["Enabled"] = False
 
@@ -93,15 +107,12 @@ e["Solver"]["Neural Network"]["Layers"][1]["Type"] = "Dense"
 e["Solver"]["Neural Network"]["Layers"][1]["Node Count"] = 20
 e["Solver"]["Neural Network"]["Layers"][1]["Activation Function"] = "Tanh"
 
-e["Solver"]["Neural Network"]["Layers"][2]["Type"] = "Dense"
-e["Solver"]["Neural Network"]["Layers"][2]["Node Count"] = 20
-e["Solver"]["Neural Network"]["Layers"][2]["Activation Function"] = "Tanh"
-
-e["Solver"]["Neural Network"]["Layers"][3]["Type"] = "Output"
-e["Solver"]["Neural Network"]["Layers"][3]["Node Count"] = 1
-e["Solver"]["Neural Network"]["Layers"][3]["Activation Function"] = "Identity" 
+e["Solver"]["Neural Network"]["Layers"][2]["Type"] = "Output"
+e["Solver"]["Neural Network"]["Layers"][2]["Node Count"] = 1
+e["Solver"]["Neural Network"]["Layers"][2]["Activation Function"] = "Identity" 
 
 e["Random Seed"] = 0xC0FFEE
+e["File Output"]["Enabled"] = False
 
 ### Training the neural network
 
