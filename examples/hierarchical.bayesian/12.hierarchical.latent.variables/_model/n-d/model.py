ddaaf734db1868ea3ef5d6c6f545cd8fd45b218e
Susi <kellersu@student.ethz.ch>
2020-07-08 16:52:46 +0200

    Larger change & bugfix in HSAEM/hierarchicalLatentLowlevel about the annealed sampling distribution. Also: fixed the test / priors returned are now the same.

---
+++ examples/hierarchical.bayesian/12.hierarchical.latent.variables/_model/n-d/model.py
@@ -0,0 +1,32 @@
+import load_data
+from _model import utils
+
+import numpy as np
+
+class ConditionalDistribution5():
+    ''' Model 5:
+     - multiple dimensions
+     - multiple distribution types
+     - latent variable coordinates are correlated
+     - p(datapoint | latent) is still a normal distribution N(latent, sigma**2)
+    '''
+
+    def __init__(self):
+        self._p = load_data.PopulationData()
+
+
+    def conditional_p(self, sample):
+
+        latent_vars = sample["Latent Variables"]
+        dataPoint = sample["Data Point"]
+        assert len(latent_vars) == self._p.nDimensions
+        sigma = self._p.sigma
+
+        logp = 0
+        for dim in range(self._p.nDimensions):
+            pt = dataPoint[dim]
+            mean = latent_vars[dim]
+            p = utils.univariate_gaussian_probability([mean], sigma, [pt])
+            logp += np.log(p)
+
+        sample["Conditional LogLikelihood"] = logp
